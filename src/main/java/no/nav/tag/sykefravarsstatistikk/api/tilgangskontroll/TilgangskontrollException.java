baa2bccb5e47c4d5b81bc6227b5b097b1af58dc2
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-08-19 15:53:55 +0200

    Legg til service for tilgangskontroll

---
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/tilgangskontroll/TilgangskontrollException.java
@@ -0,0 +1,7 @@
+package no.nav.tag.sykefravarsstatistikk.api.tilgangskontroll;
+
+public class TilgangskontrollException extends RuntimeException {
+    TilgangskontrollException(String msg) {
+        super(msg);
+    }
+}
