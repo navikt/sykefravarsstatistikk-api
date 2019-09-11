17c3d4e4573e6aed07221b8a238e85db3881e88e
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-09-11 15:05:09 +0200

    MaxPoolSize til 2

--- src/main/java/no/nav/tag/sykefravarsstatistikk/api/config/DatabaseConfig.java
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/config/DatabaseConfig.java
@@ -35,7 +35,7 @@ public class DatabaseConfig {
     private HikariDataSource dataSource(String user) {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(databaseUrl);
-        config.setMaximumPoolSize(5);
+        config.setMaximumPoolSize(2);
         config.setMinimumIdle(1);
         return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, mountPath, dbRole(user));
     }
