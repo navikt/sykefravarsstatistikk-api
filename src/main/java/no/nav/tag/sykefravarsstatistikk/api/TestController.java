e08c0e40324e454353e01654cef8b4de53f0536a
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-11-11 10:26:37 +0100

    Enable test-endepunkt i dev

--- src/main/java/no/nav/tag/sykefravarsstatistikk/api/TestController.java
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO DELETE ME
-@Profile("local")
+@Profile({"local", "dev"})
 @Unprotected
 @RestController
 public class TestController {
