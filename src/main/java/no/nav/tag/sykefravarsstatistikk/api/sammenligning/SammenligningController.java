682166cff2ffbb97088902697771ed5feacf0b7d
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-12-04 09:41:05 +0100

    Legg på cookie

--- src/main/java/no/nav/tag/sykefravarsstatistikk/api/sammenligning/SammenligningController.java
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/sammenligning/SammenligningController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
 
 @Protected
 @RestController
@@ -20,8 +25,10 @@ public class SammenligningController {
     private final SammenligningService service;
     private final TilgangskontrollService tilgangskontrollService;
 
+    private final static String STATISTIKK_ID_COOKIE_NAME = "statistikk-id";
+
     @Autowired
-    public SammenligningController(SammenligningService service, TilgangskontrollService tilgangskontrollService){
+    public SammenligningController(SammenligningService service, TilgangskontrollService tilgangskontrollService) {
         this.service = service;
         this.tilgangskontrollService = tilgangskontrollService;
     }
@@ -29,10 +36,12 @@ public class SammenligningController {
     @GetMapping(value = "/{orgnr}/sammenligning")
     public Sammenligning sammenligning(
             @PathVariable("orgnr") String orgnrStr,
-            HttpServletRequest request
+            HttpServletRequest request,
+            HttpServletResponse response
     ) {
         Orgnr orgnr = new Orgnr(orgnrStr);
         utførTilgangskontroll(orgnr, request);
+        String statistikkId = settPåCookieOgReturnerStatistikkId(request, response);
         return service.hentSammenligningForUnderenhet(orgnr);
     }
 
@@ -42,6 +51,28 @@ public class SammenligningController {
                 request.getMethod(),
                 "" + request.getRequestURL()
         );
+    }
+
+    private String settPåCookieOgReturnerStatistikkId(HttpServletRequest request, HttpServletResponse response) {
+        Optional<Cookie> cookie = getStatistikkIdCookie(request);
+
+        if (cookie.isPresent()) {
+            return cookie.get().getValue();
+        } else {
+            return setStatistikkIdCookie(response);
+        }
+    }
+
+    private Optional<Cookie> getStatistikkIdCookie(HttpServletRequest request) {
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> STATISTIKK_ID_COOKIE_NAME.equals(cookie.getName()))
+                .findAny();
+    }
+
+    private String setStatistikkIdCookie(HttpServletResponse response) {
+        String statistikkId = UUID.randomUUID().toString();
+        response.addCookie(new Cookie(STATISTIKK_ID_COOKIE_NAME, statistikkId));
+        return statistikkId;
     }
 
 }
