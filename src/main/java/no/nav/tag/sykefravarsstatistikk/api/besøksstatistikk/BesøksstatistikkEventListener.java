074c4cff9ed7f6075584384925c08f10293f5f4e
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-12-06 13:44:14 +0100

    Gi besøksstatistikk til influxdb

--- src/main/java/no/nav/tag/sykefravarsstatistikk/api/besøksstatistikk/BesøksstatistikkEventListener.java
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/besøksstatistikk/BesøksstatistikkEventListener.java
@@ -2,6 +2,11 @@ package no.nav.tag.sykefravarsstatistikk.api.besøksstatistikk;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.metrics.MetricsFactory;
+import no.nav.tag.sykefravarsstatistikk.api.domene.sammenligning.Sammenligning;
+import no.nav.tag.sykefravarsstatistikk.api.domene.sammenligning.Sykefraværprosent;
+import no.nav.tag.sykefravarsstatistikk.api.domene.virksomhetsklassifikasjoner.Næring;
+import no.nav.tag.sykefravarsstatistikk.api.domene.virksomhetsklassifikasjoner.Sektor;
+import no.nav.tag.sykefravarsstatistikk.api.enhetsregisteret.Enhet;
 import no.nav.tag.sykefravarsstatistikk.api.enhetsregisteret.Underenhet;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -23,26 +28,49 @@ public class BesøksstatistikkEventListener {
         String sessionId = sammenligningEvent.getSessionId();
         Underenhet underenhet = sammenligningEvent.getUnderenhet();
 
-        MetricsFactory.createEvent("sykefravarsstatistikk.testevent")
-                .addTagToReport("sessionId", sessionId)
-                .report();
-
         if (besøksstatistikkRepository.sessionHarBlittRegistrert(sessionId, underenhet.getOrgnr())) {
             return;
         }
 
         if (kanLagreBesøksdata(sammenligningEvent)) {
+
+            Sammenligning sammenligning = sammenligningEvent.getSammenligning();
+            Sykefraværprosent virksomhet = sammenligning.getVirksomhet();
+            Næring næring2siffer = sammenligningEvent.getNæring2siffer();
+            Enhet enhet = sammenligningEvent.getEnhet();
+            Sektor ssbSektor = sammenligningEvent.getSsbSektor();
+
             besøksstatistikkRepository.lagreBesøkFraStorVirksomhet(
                     sammenligningEvent.getEnhet(),
                     underenhet,
                     sammenligningEvent.getSsbSektor(),
                     sammenligningEvent.getNæring5siffer(),
-                    sammenligningEvent.getNæring2siffer(),
-                    sammenligningEvent.getSammenligning(),
+                    næring2siffer,
+                    sammenligning,
                     sammenligningEvent.getSessionId()
             );
+
+            MetricsFactory.createEvent("sykefravarsstatistikk.stor-bedrift.besok")
+                    .addTagToReport("arstall", String.valueOf(sammenligning.getÅrstall()))
+                    .addTagToReport("kvartal", String.valueOf(sammenligning.getKvartal()))
+                    .addTagToReport("sykefravarsprosent", String.valueOf(virksomhet.getProsent()))
+                    .addTagToReport("sykefravarsprosent_antall_personer", String.valueOf(virksomhet.getAntallPersoner()))
+                    .addTagToReport("naring_2siffer_sykefravarsprosent", String.valueOf(sammenligning.getNæring().getProsent()))
+                    .addTagToReport("ssb_sektor_sykefravarsprosent", String.valueOf(sammenligning.getSektor().getProsent()))
+                    .addTagToReport("antall_ansatte", String.valueOf(sammenligningEvent.getUnderenhet().getAntallAnsatte()))
+                    .addTagToReport("naring_5siffer_kode", underenhet.getNæringskode().getKode())
+                    .addTagToReport("naring_5siffer_beskrivelse", underenhet.getNæringskode().getBeskrivelse())
+                    .addTagToReport("naring_2siffer_beskrivelse", næring2siffer.getNavn())
+                    .addTagToReport("institusjonell_sektor_kode", enhet.getInstitusjonellSektorkode().getKode())
+                    .addTagToReport("institusjonell_sektor_beskrivelse", enhet.getInstitusjonellSektorkode().getBeskrivelse())
+                    .addTagToReport("ssb_sektor_kode", ssbSektor.getKode())
+                    .addTagToReport("ssb_sektor_beskrivelse", ssbSektor.getNavn())
+                    .report();
+
         } else {
             besøksstatistikkRepository.lagreBesøkFraLitenVirksomhet(sammenligningEvent.getSessionId());
+
+            MetricsFactory.createEvent("sykefravarsstatistikk.liten-bedrift.besok").report();
         }
     }
 
