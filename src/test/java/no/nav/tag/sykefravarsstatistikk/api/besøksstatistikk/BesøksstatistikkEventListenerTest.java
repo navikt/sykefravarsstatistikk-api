86189294e0d9dab6d6c6f669087c93d853fc8d83
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-12-06 13:48:04 +0100

    Fiks feilende test

--- src/test/java/no/nav/tag/sykefravarsstatistikk/api/besøksstatistikk/BesøksstatistikkEventListenerTest.java
+++ src/test/java/no/nav/tag/sykefravarsstatistikk/api/besøksstatistikk/BesøksstatistikkEventListenerTest.java
@@ -47,7 +47,7 @@ public class BesøksstatistikkEventListenerTest {
         sammenligning.setVirksomhet(enSykefraværprosent(4));
 
         SammenligningEvent event = enSammenligningEventBuilder()
-                .underenhet(Underenhet.builder().antallAnsatte(1000).build())
+                .underenhet(enUnderenhetBuilder().antallAnsatte(1000).build())
                 .sammenligning(sammenligning)
                 .build();
 
@@ -62,7 +62,7 @@ public class BesøksstatistikkEventListenerTest {
         Sammenligning sammenligning = enSammenligning();
         sammenligning.setVirksomhet(enSykefraværprosent(4000));
         SammenligningEvent event = enSammenligningEventBuilder()
-                .underenhet(Underenhet.builder().antallAnsatte(4).build())
+                .underenhet(enUnderenhetBuilder().antallAnsatte(4).build())
                 .build();
 
         eventListener.onSammenligningUtsendt(event);
