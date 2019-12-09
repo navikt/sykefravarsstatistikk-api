fbeb260f6e53a8e09c472bb816dfe1caba2cb5d8
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-12-09 10:39:18 +0100

    Trekk ut funksjonalitet i enklere funksjoner

--- src/main/java/no/nav/tag/sykefravarsstatistikk/api/besøksstatistikk/BesøksstatistikkEventListener.java
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/besøksstatistikk/BesøksstatistikkEventListener.java
@@ -2,6 +2,7 @@ package no.nav.tag.sykefravarsstatistikk.api.besøksstatistikk;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.metrics.MetricsFactory;
+import no.nav.tag.sykefravarsstatistikk.api.domene.Orgnr;
 import no.nav.tag.sykefravarsstatistikk.api.domene.sammenligning.Sammenligning;
 import no.nav.tag.sykefravarsstatistikk.api.domene.sammenligning.Sykefraværprosent;
 import no.nav.tag.sykefravarsstatistikk.api.domene.virksomhetsklassifikasjoner.Næring;
@@ -28,60 +29,71 @@ public class BesøksstatistikkEventListener {
     @EventListener
     public void onSammenligningUtsendt(SammenligningEvent sammenligningEvent) {
         String sessionId = sammenligningEvent.getSessionId();
-        Underenhet underenhet = sammenligningEvent.getUnderenhet();
+        Orgnr orgnr = sammenligningEvent.getUnderenhet().getOrgnr();
 
-        if (besøksstatistikkRepository.sessionHarBlittRegistrert(sessionId, underenhet.getOrgnr())) {
+        if (besøksstatistikkRepository.sessionHarBlittRegistrert(sessionId, orgnr)) {
             return;
         }
 
         if (kanLagreBesøksdata(sammenligningEvent)) {
-
-            Sammenligning sammenligning = sammenligningEvent.getSammenligning();
-            Sykefraværprosent virksomhet = sammenligning.getVirksomhet();
-            Næring næring2siffer = sammenligningEvent.getNæring2siffer();
-            Enhet enhet = sammenligningEvent.getEnhet();
-            Sektor ssbSektor = sammenligningEvent.getSsbSektor();
-
-            besøksstatistikkRepository.lagreBesøkFraStorVirksomhet(
-                    sammenligningEvent.getEnhet(),
-                    underenhet,
-                    sammenligningEvent.getSsbSektor(),
-                    sammenligningEvent.getNæring5siffer(),
-                    næring2siffer,
-                    sammenligning,
-                    sammenligningEvent.getSessionId()
-            );
-
-            BigDecimal prosentVirksomhet = sammenligning.getVirksomhet().getProsent();
-            BigDecimal prosentNæring = sammenligning.getNæring().getProsent();
-
-            boolean virksomhetErOverSnittetINæringen = prosentVirksomhet.compareTo(prosentNæring) > 0;
-
-            MetricsFactory.createEvent("sykefravarsstatistikk.stor-bedrift.besok")
-                    .addTagToReport("arstall", String.valueOf(sammenligning.getÅrstall()))
-                    .addTagToReport("kvartal", String.valueOf(sammenligning.getKvartal()))
-                    .addTagToReport("naring_5siffer_kode", underenhet.getNæringskode().getKode())
-                    .addTagToReport("naring_5siffer_beskrivelse", underenhet.getNæringskode().getBeskrivelse())
-                    .addTagToReport("naring_2siffer_beskrivelse", næring2siffer.getNavn())
-                    .addTagToReport("institusjonell_sektor_kode", enhet.getInstitusjonellSektorkode().getKode())
-                    .addTagToReport("institusjonell_sektor_beskrivelse", enhet.getInstitusjonellSektorkode().getBeskrivelse())
-                    .addTagToReport("ssb_sektor_kode", ssbSektor.getKode())
-                    .addTagToReport("ssb_sektor_beskrivelse", ssbSektor.getNavn())
-                    .addTagToReport("sykefravarsprosent_antall_personer", String.valueOf(virksomhet.getAntallPersoner()))
-                    .addTagToReport("naring_2siffer_sykefravarsprosent", String.valueOf(prosentNæring))
-                    .addTagToReport("ssb_sektor_sykefravarsprosent", String.valueOf(sammenligning.getSektor().getProsent()))
-                    .addTagToReport("sykefravarsprosent_over_naring_snitt", virksomhetErOverSnittetINæringen ? "true" : "false")
-
-                    .addFieldToReport("sykefravarsprosent", prosentVirksomhet)
-                    .addFieldToReport("antall_ansatte", underenhet.getAntallAnsatte())
-
-                    .report();
-
+            lagreBesøksdataForStorVirksomhetIDatabase(sammenligningEvent);
+            sendEventForStorVirksomhetTilInfluxDB(sammenligningEvent);
         } else {
-            besøksstatistikkRepository.lagreBesøkFraLitenVirksomhet(sammenligningEvent.getSessionId());
-
-            MetricsFactory.createEvent("sykefravarsstatistikk.liten-bedrift.besok").report();
+            lagreBesøkForLitenVirksomhetIDatabase(sammenligningEvent);
+            sendEventForLitenVirksomhetTilInfluxDB();
         }
+    }
+
+    private void lagreBesøkForLitenVirksomhetIDatabase(SammenligningEvent sammenligningEvent) {
+        besøksstatistikkRepository.lagreBesøkFraLitenVirksomhet(sammenligningEvent.getSessionId());
+    }
+
+    private void lagreBesøksdataForStorVirksomhetIDatabase(SammenligningEvent sammenligningEvent) {
+        besøksstatistikkRepository.lagreBesøkFraStorVirksomhet(
+                sammenligningEvent.getEnhet(),
+                sammenligningEvent.getUnderenhet(),
+                sammenligningEvent.getSsbSektor(),
+                sammenligningEvent.getNæring5siffer(),
+                sammenligningEvent.getNæring2siffer(),
+                sammenligningEvent.getSammenligning(),
+                sammenligningEvent.getSessionId()
+        );
+    }
+
+    private void sendEventForLitenVirksomhetTilInfluxDB() {
+        MetricsFactory.createEvent("sykefravarsstatistikk.liten-bedrift.besok").report();
+    }
+
+    private void sendEventForStorVirksomhetTilInfluxDB(SammenligningEvent sammenligningEvent) {
+        Sammenligning sammenligning = sammenligningEvent.getSammenligning();
+        Enhet enhet = sammenligningEvent.getEnhet();
+        Sektor ssbSektor = sammenligningEvent.getSsbSektor();
+        Underenhet underenhet = sammenligningEvent.getUnderenhet();
+
+        BigDecimal prosentVirksomhet = sammenligning.getVirksomhet().getProsent();
+        BigDecimal prosentNæring = sammenligning.getNæring().getProsent();
+
+        boolean virksomhetErOverSnittetINæringen = prosentVirksomhet.compareTo(prosentNæring) > 0;
+
+        MetricsFactory.createEvent("sykefravarsstatistikk.stor-bedrift.besok")
+                .addTagToReport("arstall", String.valueOf(sammenligning.getÅrstall()))
+                .addTagToReport("kvartal", String.valueOf(sammenligning.getKvartal()))
+                .addTagToReport("naring_5siffer_kode", underenhet.getNæringskode().getKode())
+                .addTagToReport("naring_5siffer_beskrivelse", underenhet.getNæringskode().getBeskrivelse())
+                .addTagToReport("naring_2siffer_beskrivelse", sammenligningEvent.getNæring2siffer().getNavn())
+                .addTagToReport("institusjonell_sektor_kode", enhet.getInstitusjonellSektorkode().getKode())
+                .addTagToReport("institusjonell_sektor_beskrivelse", enhet.getInstitusjonellSektorkode().getBeskrivelse())
+                .addTagToReport("ssb_sektor_kode", ssbSektor.getKode())
+                .addTagToReport("ssb_sektor_beskrivelse", ssbSektor.getNavn())
+                .addTagToReport("sykefravarsprosent_antall_personer", String.valueOf(sammenligning.getVirksomhet().getAntallPersoner()))
+                .addTagToReport("naring_2siffer_sykefravarsprosent", String.valueOf(prosentNæring))
+                .addTagToReport("ssb_sektor_sykefravarsprosent", String.valueOf(sammenligning.getSektor().getProsent()))
+                .addTagToReport("sykefravarsprosent_over_naring_snitt", virksomhetErOverSnittetINæringen ? "true" : "false")
+
+                .addFieldToReport("sykefravarsprosent", prosentVirksomhet)
+                .addFieldToReport("antall_ansatte", underenhet.getAntallAnsatte())
+
+                .report();
     }
 
     private boolean kanLagreBesøksdata(SammenligningEvent event) {
