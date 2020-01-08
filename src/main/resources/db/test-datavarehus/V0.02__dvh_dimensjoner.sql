c51d6722f853946596a869519f1d00b47da8bd4c
Lars Andreas van Woensel Kooy Tveiten <t152994@Larss-MacBook-Pro.local>
2020-01-08 13:43:46 +0100

    Legg til testdata for 5siffer-dimensjon

--- src/main/resources/db/test-datavarehus/V0.02__dvh_dimensjoner.sql
+++ src/main/resources/db/test-datavarehus/V0.02__dvh_dimensjoner.sql
@@ -7,8 +7,18 @@ insert into dt_p.v_dim_ia_sektor (sektorkode,sektornavn) values ('2','Kommunal f
 insert into dt_p.v_dim_ia_sektor (sektorkode,sektornavn) values ('3','Privat og offentlig næringsvirksomhet');
 insert into dt_p.v_dim_ia_sektor (sektorkode,sektornavn) values ('9','Fylkeskommunal forvaltning');
 
--- Næring
+-- Næring 2 siffer
 insert into dt_p.v_dim_ia_naring_sn2007 (naringkode,nargrpkode,naringnavn)
  values ('11','C','Utvinning av råolje og naturgass. Tjenester tilknyttet olje- og gassutvinning');
 insert into dt_p.v_dim_ia_naring_sn2007 (naringkode,nargrpkode,naringnavn)
  values ('14','B','Jordbruk, skogbruk og fiske');
+
+-- Næring 5 siffer
+insert into dt_p.dim_ia_naring (naering_kode, naering_besk_land, gruppe1_kode, gruppe1_besk_lang, gruppe2_kode, gruppe2_besk_lang, gruppe3_kode, gruppe3_besk_lang, gruppe4_kode, gruppe4_besk_lang)
+values ('33160','Reparasjon og vedlikehold av luftfartøyer og romfartøyer','3316','Reparasjon og vedlikehold av luftfartøyer og romfartøyer','331','Reparasjon av metallvarer, maskiner og utstyr','33','Reparasjon og installasjon av maskiner og utstyr','10','Industri');
+insert into dt_p.dim_ia_naring (naering_kode, naering_besk_land, gruppe1_kode, gruppe1_besk_lang, gruppe2_kode, gruppe2_besk_lang, gruppe3_kode, gruppe3_besk_lang, gruppe4_kode, gruppe4_besk_lang)
+values ('33190', 'Reparasjon av annet utstyr','3319','Reparasjon av annet utstyr','331','Reparasjon av metallvarer, maskiner og utstyr','33','Reparasjon og installasjon av maskiner og utstyr','10','Industri');
+insert into dt_p.dim_ia_naring (naering_kode, naering_besk_land, gruppe1_kode, gruppe1_besk_lang, gruppe2_kode, gruppe2_besk_lang, gruppe3_kode, gruppe3_besk_lang, gruppe4_kode, gruppe4_besk_lang)
+values ('33200','Installasjon av industrimaskiner og -utstyr','3320','Installasjon av industrimaskiner og -utstyr','332','Installasjon av industrimaskiner og -utstyr','33','Reparasjon og installasjon av maskiner og utstyr','10','Industri');
+insert into dt_p.dim_ia_naring (naering_kode, naering_besk_land, gruppe1_kode, gruppe1_besk_lang, gruppe2_kode, gruppe2_besk_lang, gruppe3_kode, gruppe3_besk_lang, gruppe4_kode, gruppe4_besk_lang)
+values ('35111','Produksjon av elektrisitet fra vannkraft','3511','Produksjon av elektrisitet','351','Produksjon, overføring og distribusjon av elektrisitet','35','Elektrisitets-, gass-, damp- og varmtvannsforsyning','35','Elektrisitet-, vann og renovasjon');
