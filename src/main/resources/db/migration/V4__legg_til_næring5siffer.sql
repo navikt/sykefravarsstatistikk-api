5827e33c0484706010a0533947f1e87fe3bb051a
Lars Andreas van Woensel Kooy Tveiten <t152994@Larss-MacBook-Pro.local>
2020-01-10 11:34:29 +0100

    Fjern grupperingstable

--- src/main/resources/db/migration/V4__legg_til_næring5siffer.sql
+++ src/main/resources/db/migration/V4__legg_til_næring5siffer.sql
@@ -8,16 +8,3 @@ create table sykefravar_statistikk_naring5siffer (
   mulige_dagsverk numeric(17,6) not null,
   opprettet timestamp default current_timestamp
 );
-
-create table naringsgruppering (
-  kode_5siffer varchar(5) primary key,
-  beskrivelse_5siffer varchar(255) not null,
-  kode_4siffer varchar(4) not null,
-  beskrivelse_4siffer varchar(255) not null,
-  kode_3siffer varchar(3) not null,
-  beskrivelse_3siffer varchar(255) not null,
-  kode_2siffer varchar(2) not null,
-  beskrivelse_2siffer varchar(255) not null,
-  kode_hovedomrade varchar not null,
-  beskrivelse_hovedomrade varchar(255) not null,
-);
