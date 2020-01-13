50e64c1c3d452a2dc072a4f1b13c3187864465de
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-01-13 16:14:01 +0100

    buggy migrering

--- src/main/resources/db/migration/V6__besøksstatistikk_legg_til_bransje.sql
+++ src/main/resources/db/migration/V6__besøksstatistikk_legg_til_bransje.sql
@@ -1,2 +1,2 @@
-alter table besoksstatistikk_virksomhet add column bransje_navn;
+alter table besoksstatistikk_virksomhet add column bransje_navn varchar;
 alter table besoksstatistikk_virksomhet add column bransje_sykefravarsprosent numeric(17,6);
