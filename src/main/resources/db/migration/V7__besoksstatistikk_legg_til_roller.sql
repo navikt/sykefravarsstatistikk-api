3857151a38d35715d9e9890ddd08604ad311c333
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-01-15 10:49:50 +0100

    Fix: feil syntax i DB migrering script

--- src/main/resources/db/migration/V7__besoksstatistikk_legg_til_roller.sql
+++ src/main/resources/db/migration/V7__besoksstatistikk_legg_til_roller.sql
@@ -1,7 +1,7 @@
 create table besoksstatistikk_unikt_besok (
  id serial primary key,
  ar smallint not null,
- uke smallint not null,
+ uke smallint not null
 );
 
 create table besoksstatistikk_altinn_roller (
