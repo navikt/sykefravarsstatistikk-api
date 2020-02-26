3a8014100e31fe531eb9fa33e03bcb0b9c89d24c
MalazAlkoj <malaz.alkoj@nav.no>
2020-02-26 14:27:26 +0100

    lage en alert for error og en test endepunkt

--- src/main/java/no/nav/tag/sykefravarsstatistikk/api/feratureToggles/FeatureToggleController.java
+++ src/main/java/no/nav/tag/sykefravarsstatistikk/api/feratureToggles/FeatureToggleController.java
@@ -1,6 +1,8 @@
 package no.nav.tag.sykefravarsstatistikk.api.feratureToggles;
 
 import no.nav.security.oidc.api.Unprotected;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.Cookie;
@@ -22,6 +25,7 @@ public class FeatureToggleController {
     private final FeatureToggleService featureToggleService;
     private final String UNLEASH_SESSION_COOKIE_NAME = "unleash-session";
 
+    private static Logger logger = LoggerFactory.getLogger(ResponseEntityExceptionHandler.class);
     @Autowired
     public FeatureToggleController(FeatureToggleService featureToggleService) {
         this.featureToggleService = featureToggleService;
@@ -43,6 +47,12 @@ public class FeatureToggleController {
         Map<String, Boolean> toggles = featureToggleService.hentFeatureToggles(features, sessionId);
         return ResponseEntity.status(HttpStatus.OK).body(toggles);
 
+    }
+
+    @GetMapping("/testfeil")
+    public ResponseEntity<Object> responseEntityFeilLogger(){
+        logger.error("Uhåndtert feil for test error logg");
+        return null;
     }
 
 }
