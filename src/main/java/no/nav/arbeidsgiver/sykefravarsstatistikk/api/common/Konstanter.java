b4cfab2ddddfe0ec3d4d8762f1fbf1683f97627b
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-04-01 13:43:26 +0200

    Legg til consumerid og correlationid på kall til altinn (#65)

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/common/Konstanter.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/common/Konstanter.java
@@ -2,4 +2,7 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.common;
 
 public class Konstanter {
     public static final int MINIMUM_ANTALL_PERSONER_SOM_SKAL_TIL_FOR_AT_STATISTIKKEN_IKKE_ER_PERSONOPPLYSNINGER = 5;
+
+    public static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-ID";
+    public static final String CONSUMER_ID_HEADER_NAME = "X-Consumer-ID";
 }
