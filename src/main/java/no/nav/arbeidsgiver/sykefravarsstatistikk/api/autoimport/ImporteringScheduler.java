416cae6eb1ad21720eebeed00b0ab1e38315bf3c
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-07-27 13:51:29 +0200

    Legg til skall av scheduler for autoimport
    * Legg til enkel shedjobb som skriver ut til logg
    
    * Logg noe det er enkelt å søke etter
    
    * Deploy
    
    * Legg til riktig cron-uttrykk og skriv bedre loggmelding

---
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/autoimport/ImporteringScheduler.java
@@ -0,0 +1,35 @@
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.autoimport;
+
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockingTaskExecutor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Component
+@Slf4j
+public class ImporteringScheduler {
+    private final LockingTaskExecutor taskExecutor;
+
+    public ImporteringScheduler(LockingTaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
+
+    @Scheduled(cron = "5 8 * * * ?")
+    public void scheduledImportering() {
+        Duration lockAtMostUntil = Duration.of(1, ChronoUnit.HOURS);
+        Duration lockAtLeastUntil = Duration.of(1, ChronoUnit.MINUTES);
+        taskExecutor.executeWithLock(
+                (Runnable) this::importering,
+                new LockConfiguration("importering", lockAtMostUntil, lockAtLeastUntil)
+        );
+    }
+
+    private void importering() {
+        log.info("Jobb for å importere sykefraværsstatistikk er startet. Selve importeringen er skrudd av for øyeblikket.");
+    }
+
+}
