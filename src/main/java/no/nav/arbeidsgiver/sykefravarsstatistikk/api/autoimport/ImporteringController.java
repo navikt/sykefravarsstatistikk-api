c2f59da948e6c1a6b198c8cf898f48ae898bebae
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-07-28 15:44:41 +0200

    Endepunkt som kan starte import av statistikk

---
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/autoimport/ImporteringController.java
@@ -0,0 +1,28 @@
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.autoimport;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.security.token.support.core.api.Protected;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Protected
+@RestController
+@Slf4j
+@RequestMapping(value = "importering")
+@Profile({"local", "dev"})
+public class ImporteringController {
+
+    private final ImporteringService importeringService;
+
+    public ImporteringController(ImporteringService importeringService) {
+        this.importeringService = importeringService;
+    }
+
+    @PostMapping(value = "/statistikk")
+    public void importStatistikk() {
+        importeringService.importerHvisDetFinnesNyStatistikk();
+    }
+}
