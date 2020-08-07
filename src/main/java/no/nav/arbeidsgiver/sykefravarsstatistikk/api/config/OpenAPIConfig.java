5a987724d1665b2fd914a76400b51c0efd3cd27d
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-08-07 14:28:59 +0200

    Oppdater config

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/config/OpenAPIConfig.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/config/OpenAPIConfig.java
@@ -6,20 +6,16 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info().title("SpringShop API")
-                        .description("Spring shop sample application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .info(new Info().title("API for sykefraværsstatistikk"))
                 .externalDocs(new ExternalDocumentation()
-                        .description("SpringShop Wiki Documentation")
-                        .url("https://springshop.wiki.github.org/docs"));
+                        .description("Repo på github")
+                        .url("https://github.com/navikt/sykefravarsstatistikk-api"));
     }
 }
