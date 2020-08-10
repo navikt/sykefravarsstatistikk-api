940eb9bcf4d32b7734bf50009212931c3e93f579
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-08-10 10:30:46 +0200

    Fjern unused import

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/config/OpenAPIConfig.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/config/OpenAPIConfig.java
@@ -3,7 +3,6 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.config;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
