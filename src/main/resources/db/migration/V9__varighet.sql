d2628de8411fdefeaef3b4b6fbbe6518c7a1a884
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-08-11 16:34:16 +0200

    Endre index

--- src/main/resources/db/migration/V9__varighet.sql
+++ src/main/resources/db/migration/V9__varighet.sql
@@ -1,1 +1,5 @@
 alter table sykefravar_statistikk_virksomhet add column varighet varchar(1);
+
+DROP INDEX orgnr_arstall_kvartal__index;
+CREATE UNIQUE INDEX orgnr_arstall_kvartal_varighet__index
+  ON sykefravar_statistikk_virksomhet (orgnr, arstall, kvartal, varighet);
