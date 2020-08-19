e7490bb33ab2ababaabc2b00bd9dba18695beb10
Malaz Alkoj <malaz.alkoj@nav.no>
2020-08-19 16:29:58 +0200

    hent ut data med varighet til med korttid og langtid

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/varighet/VarighetService.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/varighet/VarighetService.java
@@ -1,10 +1,16 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.varighet;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.common.Sykefraværsvarighet;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.domene.statistikk.ÅrstallOgKvartal;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.enhetsregisteret.Underenhet;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.sykefraværshistorikk.KvartalsvisSykefravær;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -20,9 +26,51 @@ public class VarighetService {
 
     public LangtidOgKorttidsSykefraværshistorikk hentLangtidOgKorttidsSykefraværshistorikk(Underenhet underenhet) {
 
-        List<KvartalsvisSykefraværMedVarighet> sykefraværVarighet = kvartalsvisSykefraværVarighetRepository.hentKvartalsvisSykefraværMedVarighet(
-                underenhet);
+        List<KvartalsvisSykefraværMedVarighet> sykefraværVarighet =
+                kvartalsvisSykefraværVarighetRepository.hentKvartalsvisSykefraværMedVarighet(
+                        underenhet);
+        ÅrstallOgKvartal årstallOgKvartal = sykefraværVarighet.get(0).getÅrstallOgKvartal();
+        BigDecimal muligeDagsverk = sykefraværVarighet.stream()
+                .filter(p ->
+                        p.getTypeVarighet().equals(Sykefraværsvarighet.TOTAL.kode)
+                ).collect(Collectors.toList())
+                .get(0)
+                .getMuligeDagsverk();
+        int antallPers = sykefraværVarighet.stream()
+                .filter(p ->
+                        p.getTypeVarighet().equals(Sykefraværsvarighet.TOTAL.kode)
+                ).collect(Collectors.toList())
+                .get(0)
+                .getAntallPersoner();
+        BigDecimal tapteDagsverkKorttid = sykefraværVarighet.stream()
+                .filter(p ->
+                        p.getTypeVarighet().equals(Sykefraværsvarighet._1_DAG_TIL_7_DAGER.kode)
+                ).collect(Collectors.toList())
+                .get(0)
+                .getTapteDagsverk();
+        BigDecimal tapteDagsverkLangtid = sykefraværVarighet.stream()
+                .filter(p ->
+                        p.getTypeVarighet().equals(Sykefraværsvarighet.MER_ENN_39_UKER.kode)
+                ).collect(Collectors.toList())
+                .get(0)
+                .getTapteDagsverk();
 
-        return null;
+        SykefraværMedVarighetshistorikk korttid = new SykefraværMedVarighetshistorikk();
+        korttid.setVarighet("korttid");
+        korttid.setKvartalsvisSykefravær(Arrays.asList(
+                new KvartalsvisSykefravær
+                        (årstallOgKvartal,tapteDagsverkKorttid,muligeDagsverk,antallPers)
+        ));
+        SykefraværMedVarighetshistorikk langtid = new SykefraværMedVarighetshistorikk();
+        langtid.setVarighet("langtid");
+        langtid.setKvartalsvisSykefravær(Arrays.asList(
+                new KvartalsvisSykefravær
+                        (årstallOgKvartal,tapteDagsverkLangtid,muligeDagsverk,antallPers)
+        ));
+        LangtidOgKorttidsSykefraværshistorikk langtidOgKorttidsSykefraværshistorikk =
+                new LangtidOgKorttidsSykefraværshistorikk();
+        langtidOgKorttidsSykefraværshistorikk.setKorttidssykefravær(korttid);
+        langtidOgKorttidsSykefraværshistorikk.setLangtidssykefravær(langtid);
+        return langtidOgKorttidsSykefraværshistorikk;
     }
 }
--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/varighet/VarighetServiceTest.java
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/varighet/VarighetServiceTest.java
@@ -1,6 +1,8 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.varighet;
 
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.common.Sykefraværsvarighet;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.domene.Orgnr;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.domene.statistikk.ÅrstallOgKvartal;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.enhetsregisteret.Næringskode5Siffer;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.enhetsregisteret.Underenhet;
 import org.junit.Before;
@@ -15,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,7 +52,6 @@ public class VarighetServiceTest {
     }
 
 
-    @Disabled
     @Test
     public void hentLangtidOgKorttidsSykefraværshistorikk() {
         LangtidOgKorttidsSykefraværshistorikk langtidOgKorttidsSykefraværshistorikk =
@@ -59,14 +62,40 @@ public class VarighetServiceTest {
         SykefraværMedVarighetshistorikk langtidssykefravær =
                 langtidOgKorttidsSykefraværshistorikk.getLangtidssykefravær();
 
-        assertThat(korttidssykefravær.getKvartalsvisSykefravær().size()).isEqualTo(4);
+        assertThat(korttidssykefravær.getKvartalsvisSykefravær().size()).isEqualTo(1);
+        assertThat(korttidssykefravær.getKvartalsvisSykefravær().get(0).getTapteDagsverk()).isEqualTo(BigDecimal.valueOf(5.0));
         assertThat(korttidssykefravær.getVarighet()).isEqualTo("korttid");
-        assertThat(langtidssykefravær.getKvartalsvisSykefravær().size()).isEqualTo(4);
+        assertThat(langtidssykefravær.getKvartalsvisSykefravær().size()).isEqualTo(1);
+        assertThat(langtidssykefravær.getKvartalsvisSykefravær().get(0).getTapteDagsverk()).isEqualTo(BigDecimal.valueOf(2.0));
         assertThat(langtidssykefravær.getVarighet()).isEqualTo("langtid");
     }
 
     private List<KvartalsvisSykefraværMedVarighet> aUtilityMethod() {
-        return null;
+        return Arrays.asList(
+                new KvartalsvisSykefraværMedVarighet(
+                        new ÅrstallOgKvartal(2020, 1),
+                        BigDecimal.valueOf(5),
+                        BigDecimal.valueOf(0),
+                        0,
+                        Sykefraværsvarighet._1_DAG_TIL_7_DAGER.kode
+                ),
+                new KvartalsvisSykefraværMedVarighet(
+                        new ÅrstallOgKvartal(2020, 1),
+                        BigDecimal.valueOf(2),
+                        BigDecimal.valueOf(0),
+                        0,
+                        Sykefraværsvarighet.MER_ENN_39_UKER.kode
+                ),
+                new KvartalsvisSykefraværMedVarighet(
+                        new ÅrstallOgKvartal(2020, 1),
+                        BigDecimal.valueOf(0),
+                        BigDecimal.valueOf(100),
+                        10,
+                        Sykefraværsvarighet.TOTAL.kode
+                )
+
+        )
+                ;
     }
 
 }
