aee22c5e312c2866f3d0a4f3d426ea9aa93a9b9c
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-11-18 16:11:54 +0100

    Beregn SummertKorttidsOgLangtidsfravær for bransje/næring (WIP)

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/summert/VarighetService.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/summert/VarighetService.java
@@ -1,27 +1,77 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.summert;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.Varighetskategori;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.ÅrstallOgKvartal;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Næring;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Underenhet;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.bransjeprogram.Bransje;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.bransjeprogram.Bransjeprogram;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.ÅrstallOgKvartal;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.KlassifikasjonerRepository;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.Varighetskategori;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.SummertSykefravær;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.UmaskertSykefraværForEttKvartal;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.UmaskertSykefraværForEttKvartalMedVarighet;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.kvartalsvis.KvartalsvisSykefraværshistorikkService.LENGDE_PÅ_NÆRINGSKODE_AV_BRANSJENIVÅ;
 
 @Slf4j
 @Component
 public class VarighetService {
 
     private final VarighetRepository varighetRepository;
+    private final Bransjeprogram bransjeprogram;
+    private final KlassifikasjonerRepository klassifikasjonerRepository;
+
 
     public VarighetService(
-            VarighetRepository varighetRepository) {
+            VarighetRepository varighetRepository,
+            Bransjeprogram bransjeprogram,
+            KlassifikasjonerRepository klassifikasjonerRepository
+            ) {
         this.varighetRepository = varighetRepository;
+        this.bransjeprogram = bransjeprogram;
+        this.klassifikasjonerRepository = klassifikasjonerRepository;
+    }
+
+    public SummertKorttidsOgLangtidsfravær hentSummertKorttidsOgLangtidsfraværForNæring(
+            Underenhet underenhet,
+            ÅrstallOgKvartal sistePubliserteÅrstallOgKvartal,
+            int antallKvartalerSomSkalSummeres
+    ) {
+        if (antallKvartalerSomSkalSummeres < 1) {
+            throw new IllegalArgumentException("Kan ikke summere færre enn ett kvartal");
+        }
+
+        // TODO: fjern duplisering med KvartalsvisSykefraværshistorikkService
+        Optional<Bransje> bransje = bransjeprogram.finnBransje(underenhet);
+        boolean erIBransjeprogram =
+                bransje.isPresent()
+                        && bransje.get().lengdePåNæringskoder() == LENGDE_PÅ_NÆRINGSKODE_AV_BRANSJENIVÅ;
+
+
+        List<UmaskertSykefraværForEttKvartalMedVarighet> sykefraværVarighet;
+
+        if (erIBransjeprogram) {
+            sykefraværVarighet = varighetRepository.hentSykefraværForEttKvartalMedVarighet(bransje.get());
+        } else {
+            Næring næring = klassifikasjonerRepository.hentNæring(underenhet.getNæringskode().hentNæringskode2Siffer());
+            sykefraværVarighet = varighetRepository.hentSykefraværForEttKvartalMedVarighet(næring);
+        }
+
+        return getSummertKorttidsOgLangtidsfravær(
+                sistePubliserteÅrstallOgKvartal,
+                antallKvartalerSomSkalSummeres,
+                sykefraværVarighet
+        );
     }
 
     public SummertKorttidsOgLangtidsfravær hentSummertKorttidsOgLangtidsfravær(
@@ -38,6 +88,19 @@ public class VarighetService {
                         underenhet
                 );
 
+        return getSummertKorttidsOgLangtidsfravær(
+                sistePubliserteÅrstallOgKvartal,
+                antallKvartalerSomSkalSummeres,
+                sykefraværVarighet
+        );
+    }
+
+    @NotNull
+    private SummertKorttidsOgLangtidsfravær getSummertKorttidsOgLangtidsfravær(
+            ÅrstallOgKvartal sistePubliserteÅrstallOgKvartal,
+            int antallKvartalerSomSkalSummeres,
+            List<UmaskertSykefraværForEttKvartalMedVarighet> sykefraværVarighet
+    ) {
         List<ÅrstallOgKvartal> kvartalerSomSkalSummeres = ÅrstallOgKvartal.range(
                 sistePubliserteÅrstallOgKvartal.minusKvartaler(antallKvartalerSomSkalSummeres - 1),
                 sistePubliserteÅrstallOgKvartal
