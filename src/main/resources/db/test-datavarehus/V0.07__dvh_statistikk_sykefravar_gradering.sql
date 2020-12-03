2478cb5a8a79d51816b31e209207982282c4c29d
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-12-03 14:41:54 +0100

    Legg til indeks på statistikk virksomhet for graderte sykemeldinger tabell

--- src/main/resources/db/test-datavarehus/V0.07__dvh_statistikk_sykefravar_gradering.sql
+++ src/main/resources/db/test-datavarehus/V0.07__dvh_statistikk_sykefravar_gradering.sql
@@ -20,3 +20,24 @@ insert into dt_p.agg_ia_sykefravar_v_2
   78.048722, 1269.433466, 42
  );
 
+insert into dt_p.agg_ia_sykefravar_v_2
+ (
+  arstall, kvartal,
+  orgnr, naring, naering_kode,
+  alder, kjonn, fylkbo, kommnr,
+  rectype,
+  antall_gs, taptedv_gs,
+  antall,
+  taptedv, mulige_dv, antpers
+ )
+ values
+ (
+  '2020','1',
+  '987654321', '02', '02220',
+  'B', 'M', '42', '4215',
+  '2',
+  1, 3.220422,
+  7,
+  109.77722, 2345.488866, 170
+ );
+
