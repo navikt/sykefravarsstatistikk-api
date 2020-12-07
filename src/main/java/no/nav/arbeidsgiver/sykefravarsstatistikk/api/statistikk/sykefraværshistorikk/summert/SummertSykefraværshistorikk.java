cff19cfd6a248eb6ed34356e40cfe3a3775741d1
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-12-07 15:56:07 +0100

    Endepunktet /summert skal ikke returnere summertGradertFravær når det er null

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/summert/SummertSykefraværshistorikk.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/summert/SummertSykefraværshistorikk.java
@@ -1,5 +1,6 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.summert;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.Statistikkategori;
@@ -11,5 +12,7 @@ public class SummertSykefraværshistorikk {
     private Statistikkategori type;
     private String label;
     private SummertKorttidsOgLangtidsfravær summertKorttidsOgLangtidsfravær;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private SummertSykefravær summertGradertFravær;
 }
