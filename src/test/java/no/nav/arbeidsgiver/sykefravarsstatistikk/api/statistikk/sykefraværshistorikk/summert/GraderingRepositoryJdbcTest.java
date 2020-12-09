5821832dc97a41ab77d9287e509a1e61d71e9419
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-12-09 15:03:15 +0100

    Legge til summert gradert sykefravær for næring

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/summert/GraderingRepositoryJdbcTest.java
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/summert/GraderingRepositoryJdbcTest.java
@@ -1,9 +1,6 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.summert;
 
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Næringskode5Siffer;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Orgnr;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Underenhet;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.ÅrstallOgKvartal;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.*;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.UmaskertSykefraværForEttKvartal;
 import org.junit.After;
 import org.junit.Before;
@@ -26,6 +23,24 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @DataJdbcTest
 public class GraderingRepositoryJdbcTest {
+
+    private static final Næring PRODUKSJON_AV_KLÆR = new Næring("14", "Produksjon av klær");
+    private static final Næring PRODUKSJON_AV_LÆR_OG_LÆRVARER = new Næring("15", "Produksjon av lær og lærvarer");
+    private static Underenhet UNDERENHET_1_NÆRING_14 = Underenhet.builder()
+            .orgnr(new Orgnr("999999999"))
+            .næringskode(new Næringskode5Siffer("14120", "Produksjon av arbeidstøy"))
+            .build();
+    private static Underenhet UNDERENHET_2_NÆRING_15 = Underenhet.builder()
+            .orgnr(new Orgnr("888888888"))
+            .næringskode(new Næringskode5Siffer("15100", "andre_næringskode"))
+            .build();
+    private static Underenhet UNDERENHET_3_NÆRING_14 = Underenhet.builder()
+            .orgnr(new Orgnr("777777777"))
+            .næringskode(new Næringskode5Siffer("14120", "Produksjon av arbeidstøy"))
+            .build();
+    private static ÅrstallOgKvartal _2020_1 = new ÅrstallOgKvartal(2020, 1);
+    private static ÅrstallOgKvartal _2019_4 = new ÅrstallOgKvartal(2019, 4);
+
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -44,18 +59,11 @@ public class GraderingRepositoryJdbcTest {
 
     @Test
     public void hentSykefraværForEttKvartalMedGradering__skal_returnere_riktig_sykefravær() {
-        Underenhet underenhet = Underenhet.builder()
-                .orgnr(new Orgnr("999999999"))
-                .navn("bedrift med 2 næringskoder")
-                .næringskode(new Næringskode5Siffer("14100", "tilfeldigNæringskode"))
-                .antallAnsatte(10)
-                .overordnetEnhetOrgnr(new Orgnr("1111111111")).build();
         insertDataMedGradering(
                 jdbcTemplate,
-                underenhet,
-                underenhet.getNæringskode().getKode(),
-                2019,
-                4,
+                UNDERENHET_1_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(), "14100",
+                _2019_4,
                 5,
                 9,
                 7,
@@ -65,10 +73,9 @@ public class GraderingRepositoryJdbcTest {
         );
         insertDataMedGradering(
                 jdbcTemplate,
-                underenhet,
-                "14222",
-                2019,
-                4,
+                UNDERENHET_1_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(), "14222",
+                _2019_4,
                 2,
                 9,
                 7,
@@ -78,10 +85,9 @@ public class GraderingRepositoryJdbcTest {
         );
         insertDataMedGradering(
                 jdbcTemplate,
-                underenhet,
-                "14222",
-                2020,
-                1,
+                UNDERENHET_1_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(), "14222",
+                _2020_1,
                 19,
                 30,
                 15,
@@ -90,7 +96,8 @@ public class GraderingRepositoryJdbcTest {
                 new BigDecimal(300)
         );
 
-        List<UmaskertSykefraværForEttKvartal> resultat = graderingRepository.hentSykefraværForEttKvartalMedGradering(underenhet);
+        List<UmaskertSykefraværForEttKvartal> resultat =
+                graderingRepository.hentSykefraværForEttKvartalMedGradering(UNDERENHET_1_NÆRING_14);
 
         assertThat(resultat.size()).isEqualTo(2);
         assertThat(resultat.get(0)).isEqualTo(
@@ -113,24 +120,12 @@ public class GraderingRepositoryJdbcTest {
 
     @Test
     public void hentSykefraværForEttKvartalMedGradering__skal_returnere_riktig_underenhet_sykefravær() {
-        Underenhet underenhet1 = Underenhet.builder()
-                .orgnr(new Orgnr("999999999"))
-                .navn("bedrift med 2 næringskoder")
-                .næringskode(new Næringskode5Siffer("14100", "tilfeldigNæringskode"))
-                .antallAnsatte(10)
-                .overordnetEnhetOrgnr(new Orgnr("1111111111")).build();
-        Underenhet underenhet2 = Underenhet.builder()
-                .orgnr(new Orgnr("888888888"))
-                .navn("bedrift med 2 næringskoder")
-                .næringskode(new Næringskode5Siffer("15100", "andre_næringskode"))
-                .antallAnsatte(10)
-                .overordnetEnhetOrgnr(new Orgnr("1111111111")).build();
         insertDataMedGradering(
                 jdbcTemplate,
-                underenhet1,
-                underenhet1.getNæringskode().getKode(),
-                2019,
-                4,
+                UNDERENHET_1_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(),
+                UNDERENHET_1_NÆRING_14.getNæringskode().getKode(),
+                _2019_4,
                 5,
                 9,
                 7,
@@ -140,10 +135,9 @@ public class GraderingRepositoryJdbcTest {
         );
         insertDataMedGradering(
                 jdbcTemplate,
-                underenhet1,
-                "14222",
-                2020,
-                1,
+                UNDERENHET_1_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(), "14222",
+                _2020_1,
                 2,
                 9,
                 7,
@@ -153,10 +147,9 @@ public class GraderingRepositoryJdbcTest {
         );
         insertDataMedGradering(
                 jdbcTemplate,
-                underenhet2,
-                "14222",
-                2020,
-                1,
+                UNDERENHET_3_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(), "14222",
+                _2020_1,
                 19,
                 30,
                 15,
@@ -165,7 +158,7 @@ public class GraderingRepositoryJdbcTest {
                 new BigDecimal(300)
         );
 
-        List<UmaskertSykefraværForEttKvartal> resultat = graderingRepository.hentSykefraværForEttKvartalMedGradering(underenhet1);
+        List<UmaskertSykefraværForEttKvartal> resultat = graderingRepository.hentSykefraværForEttKvartalMedGradering(UNDERENHET_1_NÆRING_14);
 
         assertThat(resultat.size()).isEqualTo(2);
         assertThat(resultat.get(0)).isEqualTo(
@@ -187,12 +180,76 @@ public class GraderingRepositoryJdbcTest {
     }
 
 
+    @Test
+    public void hentSykefraværForEttKvartalMedGradering__skal_returnere_riktig_sykefravær_for_næring() {
+
+        insertDataMedGradering(
+                jdbcTemplate,
+                UNDERENHET_1_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(),
+                UNDERENHET_1_NÆRING_14.getNæringskode().getKode(),
+                _2019_4,
+                5,
+                9,
+                7,
+                new BigDecimal(10),
+                new BigDecimal(20),
+                new BigDecimal(100)
+        );
+        insertDataMedGradering(
+                jdbcTemplate,
+                UNDERENHET_3_NÆRING_14.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_KLÆR.getKode(),
+                "14222",
+                _2020_1,
+                2,
+                9,
+                7,
+                new BigDecimal(12),
+                new BigDecimal(20),
+                new BigDecimal(100)
+        );
+        insertDataMedGradering(
+                jdbcTemplate,
+                UNDERENHET_2_NÆRING_15.getOrgnr().getVerdi(),
+                PRODUKSJON_AV_LÆR_OG_LÆRVARER.getKode(),
+                "15333",
+                _2020_1,
+                19,
+                30,
+                15,
+                new BigDecimal(25),
+                new BigDecimal(50),
+                new BigDecimal(300)
+        );
+
+        List<UmaskertSykefraværForEttKvartal> resultat = graderingRepository.hentSykefraværForEttKvartalMedGradering(PRODUKSJON_AV_KLÆR);
+
+        assertThat(resultat.size()).isEqualTo(2);
+        assertThat(resultat.get(0)).isEqualTo(
+                new UmaskertSykefraværForEttKvartal(
+                        new ÅrstallOgKvartal(2019, 4),
+                        new BigDecimal(10),
+                        new BigDecimal(100),
+                        7
+                )
+        );
+        assertThat(resultat.get(1)).isEqualTo(
+                new UmaskertSykefraværForEttKvartal(
+                        new ÅrstallOgKvartal(2020, 1),
+                        new BigDecimal(12),
+                        new BigDecimal(100),
+                        7
+                )
+        );
+    }
+
     private void insertDataMedGradering(
             NamedParameterJdbcTemplate jdbcTemplate,
-            Underenhet underenhet,
+            String orgnr,
+            String næring,
             String næringskode,
-            int årstall,
-            int kvartal,
+            ÅrstallOgKvartal årstallOgKvartal,
             int antallGraderteSykemeldinger,
             int antallSykemeldinger,
             int antallPersoner,
@@ -226,11 +283,11 @@ public class GraderingRepositoryJdbcTest {
                         ":tapte_dagsverk, " +
                         ":mulige_dagsverk)",
                 parametre(
-                        underenhet.getOrgnr().getVerdi(),
-                        "14",
+                        orgnr,
+                        næring,
                         næringskode,
-                        årstall,
-                        kvartal,
+                        årstallOgKvartal.getÅrstall(),
+                        årstallOgKvartal.getKvartal(),
                         antallGraderteSykemeldinger,
                         tapteDagsverkGradertSykemelding,
                         antallSykemeldinger,
