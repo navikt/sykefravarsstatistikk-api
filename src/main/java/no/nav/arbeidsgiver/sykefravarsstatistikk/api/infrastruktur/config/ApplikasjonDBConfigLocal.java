9aee7959a3d88b6d1fc9dd4071705139f9f9b82d
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-25 11:57:26 +0100

    Revert "Set init fail timeout slik at proxy kan settes opp"
    This reverts commit 6b1eba9f29831b35be226dbb387a85f685b063bb.

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/ApplikasjonDBConfigLocal.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/ApplikasjonDBConfigLocal.java
@@ -41,8 +41,6 @@ public class ApplikasjonDBConfigLocal {
         config.setPassword(password);
         config.setMaximumPoolSize(2);
         config.setDriverClassName(driverClassName);
-        config.setInitializationFailTimeout(60000);
-        config.setDriverClassName("org.postgresql.Driver");
 
         return new HikariDataSource(config);
     }
