29c06401ba12ef51d42edd4952163aa33c5b9890
erlendaxelsson <erlend.axelsson@gmail.com>
2021-03-16 12:48:06 +0100

    Fjernet profile fra KafkaConfig

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/KafkaConfig.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/KafkaConfig.java
@@ -3,11 +3,9 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.config;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
-@Profile({"dev-gcp", "prod-gcp"})
 class KafkaConfig {
 	@Bean
 	KafkaTemplate<String, String> kafkaTemplate(KafkaProperties kafkaProperties) {
