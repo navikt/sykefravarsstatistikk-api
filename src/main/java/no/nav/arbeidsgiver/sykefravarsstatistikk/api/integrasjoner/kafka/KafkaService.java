2465bf7d03a9ab1ac15c1d0fc579ef282ed8b4b5
Malaz Alkoj <malaz.alkoj@nav.no>
2021-03-16 16:29:38 +0100

    prøve å sende kafka med et tilfeldig orgnr

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/kafka/KafkaService.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/kafka/KafkaService.java
@@ -39,7 +39,7 @@ public class KafkaService {
         Næringskode5Siffer næringskode = null;
         try {
             næringskode = enhetsregisteretClient.hentInformasjonOmVirksomhet(
-                    new Orgnr(sykefraværForEttKvartalMedOrgNr.getOrgnr())
+                    new Orgnr("925028509")
             ).getNæringskode();
         } catch (HttpClientErrorException e) {
             if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
