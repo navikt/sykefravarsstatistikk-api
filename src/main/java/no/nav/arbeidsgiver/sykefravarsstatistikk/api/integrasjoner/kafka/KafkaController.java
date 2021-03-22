ed1da622f366909c7a796ed39e4fa99c0ba95957
Malaz Alkoj <malaz.alkoj@nav.no>
2021-03-22 10:55:34 +0100

    sette endepunkt til protected og fjerne ubrukte variabel

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/kafka/KafkaController.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/kafka/KafkaController.java
@@ -3,13 +3,15 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.integrasjoner.kafka;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.ÅrstallOgKvartal;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.importering.autoimport.EksporteringService;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.SykefraværForEttKvartalMedOrgNr;
-import no.nav.security.token.support.core.api.Unprotected;
+import no.nav.security.token.support.core.api.Protected;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
-@Unprotected
+@Protected
+@Profile({"local","dev"})
 @RestController
 public class KafkaController {
     final KafkaService kafkaService;
@@ -19,14 +21,6 @@ public class KafkaController {
         this.kafkaService = kafkaService;
         this.eksporteringService = eksporteringService;
     }
-
-    SykefraværForEttKvartalMedOrgNr dummy = new SykefraværForEttKvartalMedOrgNr(
-            new ÅrstallOgKvartal(2020, 2),
-            "999999999",
-            new BigDecimal(10),
-            new BigDecimal(100),
-            51, "08500"
-    );
 
     @GetMapping(value = "/sendkafka")
     public void sendKafka() {
