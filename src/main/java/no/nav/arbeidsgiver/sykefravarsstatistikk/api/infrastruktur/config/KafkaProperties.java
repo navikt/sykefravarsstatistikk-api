c9bae25d18995066472f232c5aac90b375465e30
Malaz Alkoj <malaz.alkoj@nav.no>
2021-04-27 16:09:14 +0200

    endre LingerMS til 1

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/KafkaProperties.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/KafkaProperties.java
@@ -38,7 +38,7 @@ public class KafkaProperties {
 	private final Integer retries = Integer.MAX_VALUE;
 	private final Integer deliveryTimeoutMs = 10100;
 	private final Integer requestTimeoutMs = 10000;
-	private final Integer lingerMs = 10;
+	private final Integer lingerMs = 1;
 	private final Integer batchSize = 16384*4;
 
 	public Map<String, Object> asProperties() {
