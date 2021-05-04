ec23507e3e6025c3473ca8590cb6e1b6faf0a396
Malaz Alkoj <malaz.alkoj@nav.no>
2021-05-04 11:32:43 +0200

    gjøre controller til protected

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/eksportering/autoeksport/EksporteringController.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/eksportering/autoeksport/EksporteringController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Unprotected
+@Protected
 @RestController
 @Slf4j
 @RequestMapping(value = "eksportering")
