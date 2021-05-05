cb779cbb0ed0573a2b7cb86372fd8384d79a25ad
Malaz Alkoj <malaz.alkoj@nav.no>
2021-05-05 11:20:04 +0200

    slette virksomhetMetadata før eksportering for alle kvartaler, vil bli erstattet av en ny forbedret historikk

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/importering/autoimport/PostImporteringService.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/importering/autoimport/PostImporteringService.java
@@ -93,7 +93,7 @@ public class PostImporteringService {
 
         log.info(
                 "Importering av VirksomhetMetadata og VirksomhetNæringskode5sifferMapping er ferdig. " +
-                "'{}' VirksomhetMetadata og '{}' VirksomhetNæringskode5sifferMapping har blitt importert. ",
+                        "'{}' VirksomhetMetadata og '{}' VirksomhetNæringskode5sifferMapping har blitt importert. ",
                 antallVirksomhetMetadataOpprettet,
                 antallVirksomhetMetadataNæringskode5siffer
         );
@@ -105,7 +105,7 @@ public class PostImporteringService {
         if (!erEksporteringAktivert) {
             log.info(
                     "Eksportering er ikke aktivert. " +
-                    "Skal ikke forberedde til neste eksportering for årstall '{}' og kvartal '{}'. ",
+                            "Skal ikke forberedde til neste eksportering for årstall '{}' og kvartal '{}'. ",
                     årstallOgKvartal.getÅrstall(),
                     årstallOgKvartal.getKvartal()
             );
@@ -132,7 +132,7 @@ public class PostImporteringService {
                 mapToVirksomhetEksportPerKvartal(virksomhetMetadata);
         log.info(
                 "Skal gjøre klar '{}' virksomheter til neste eksportering. ",
-                virksomhetEksportPerKvartalListe == null? 0 : virksomhetEksportPerKvartalListe.size()
+                virksomhetEksportPerKvartalListe == null ? 0 : virksomhetEksportPerKvartalListe.size()
         );
 
         int antallOpprettet = eksporteringRepository.opprettEksport(virksomhetEksportPerKvartalListe);
@@ -151,22 +151,12 @@ public class PostImporteringService {
         }
 
         log.info("Antall orgenhet fra DVH: {}", orgenhetList.size());
-        int antallVirksomhetMetadataForÅrstallOgKvartal =
-                virksomhetMetadataRepository.hentAntallVirksomhetMetadata(årstallOgKvartal);
-
-        if (antallVirksomhetMetadataForÅrstallOgKvartal > 0) {
-            log.info("Sletter '{}' eksisterende VirksomhetMetadata for årstall '{}' og kvartal '{}' før import. ",
-                    antallVirksomhetMetadataForÅrstallOgKvartal,
-                    årstallOgKvartal.getÅrstall(),
-                    årstallOgKvartal.getKvartal()
-            );
-            int antallSlettet = virksomhetMetadataRepository.slettVirksomhetMetadata(årstallOgKvartal);
-            log.info("Slettet '{}' VirksomhetMetadata for årstall '{}' og kvartal '{}'",
-                    antallSlettet,
-                    årstallOgKvartal.getÅrstall(),
-                    årstallOgKvartal.getKvartal()
-            );
-        }
+        int antallSlettet = virksomhetMetadataRepository.slettVirksomhetMetadata();
+        log.info("Slettet '{}' VirksomhetMetadata for årstall '{}' og kvartal '{}'",
+                antallSlettet,
+                årstallOgKvartal.getÅrstall(),
+                årstallOgKvartal.getKvartal()
+        );
         int antallOpprettet =
                 virksomhetMetadataRepository.opprettVirksomhetMetadata(mapToVirksomhetMetadata(orgenhetList));
         log.info("Antall rader VirksomhetMetadata opprettet: {}", antallOpprettet);
@@ -186,7 +176,7 @@ public class PostImporteringService {
                 return Collections.emptyList();
             }
 
-            if(alleSisteTilgjengeligKvartal.size() != 1) {
+            if (alleSisteTilgjengeligKvartal.size() != 1) {
                 log.warn(
                         "Har ikke funnet Orgenhet for årstall '{}' og kvartal '{}'. " +
                                 "Flere enn 1 årstal og kvartal funnet i DVH for Orgenhet, antall: '{}'.",
@@ -220,17 +210,8 @@ public class PostImporteringService {
             return 0;
         }
 
-        int antallNæringOgNæringskode5siffer =
-                virksomhetMetadataRepository.hentAntallNæringOgNæringskode5siffer(årstallOgKvartal);
-
-        if (antallNæringOgNæringskode5siffer > 0) {
-            log.info("Sletter '{}' eksisterende NæringOgNæringskode5siffer for årstall '{}' og kvartal '{}'. ",
-                    antallNæringOgNæringskode5siffer,
-                    årstallOgKvartal.getÅrstall(),
-                    årstallOgKvartal.getKvartal()
-            );
-            virksomhetMetadataRepository.slettNæringOgNæringskode5siffer(årstallOgKvartal);
-        }
+        int antallSlettetNæringskode5Siffer = virksomhetMetadataRepository.slettNæringOgNæringskode5siffer();
+        log.info("Slettet '{}' eksisterende NæringOgNæringskode5siffer. ", antallSlettetNæringskode5Siffer);
 
         int antallOpprettet =
                 virksomhetMetadataRepository.opprettVirksomhetMetadataNæringskode5siffer(
