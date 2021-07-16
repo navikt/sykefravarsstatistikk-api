0bcaaa381376d5e1edb02c290f78ad7d779203dc
Malaz Alkoj <malaz.alkoj@nav.no>
2021-07-16 13:44:11 +0200

    enable eksport i prod midlertidig (#213)

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/eksportering/autoeksport/EksporteringController.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/eksportering/autoeksport/EksporteringController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping(value = "eksportering")
-@Profile({"local", "dev"})
+@Profile({"local", "dev", "prod"})
 public class EksporteringController {
 
     private final EksporteringService eksporteringService;
