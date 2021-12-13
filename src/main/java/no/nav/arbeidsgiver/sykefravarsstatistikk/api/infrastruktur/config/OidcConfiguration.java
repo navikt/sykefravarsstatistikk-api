9a8ae6f044a780ddb1f179d7eb5036d7f89ea9ca
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-12-13 11:39:14 +0100

    Remove profile

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/OidcConfiguration.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/OidcConfiguration.java
@@ -3,10 +3,8 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.config;
 import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!local")
 public class OidcConfiguration {
     @Bean
     public ProxyAwareResourceRetriever resourceRetriever() {
