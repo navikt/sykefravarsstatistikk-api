1c45d471ae281d746b3cc57fe4b751c00909bb26
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-12-15 10:28:12 +0100

    Logg url til enhetsregisteret

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/enhetsregisteret/EnhetsregisteretClient.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/enhetsregisteret/EnhetsregisteretClient.java
@@ -22,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class EnhetsregisteretClient {
     private final RestTemplate restTemplate;
@@ -61,6 +62,7 @@ public class EnhetsregisteretClient {
     public Underenhet hentInformasjonOmUnderenhet(Orgnr orgnrTilUnderenhet) {
         try {
             String url = enhetsregisteretUrl + "underenheter/" + orgnrTilUnderenhet.getVerdi();
+            log.info("Url til enhetsregisteret: " + url);
             String respons = restTemplate.getForObject(url, String.class);
             Underenhet underenhet = mapTilUnderenhet(respons);
             validerReturnertOrgnr(orgnrTilUnderenhet, underenhet.getOrgnr());
