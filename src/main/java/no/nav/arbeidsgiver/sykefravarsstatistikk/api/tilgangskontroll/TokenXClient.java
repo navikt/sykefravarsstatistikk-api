627236d2dc009a77f61e9b80310eed884c1812d7
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-01-06 11:34:44 +0100

    Fiks: TokenExchange med en generert jwk (i mvc-test)

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/tilgangskontroll/TokenXClient.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/tilgangskontroll/TokenXClient.java
@@ -17,7 +17,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -47,7 +50,6 @@ public class TokenXClient {
         // Henter metadata
         AuthorizationServerMetadata authorizationServerMetadata = AuthorizationServerMetadata.resolve(new Issuer(tokenxWellKnownUrl));
 
-
         // Lag assertion token
         RSAKey jwk = RSAKey.parse(tokenxJwk);
         JWSHeader header = new JWSHeader
@@ -63,90 +65,46 @@ public class TokenXClient {
                 .issuer(tokenxClientId)
                 .claim(JWTClaimNames.NOT_BEFORE, Instant.now().minus(1, ChronoUnit.MINUTES).getEpochSecond())
                 .claim(JWTClaimNames.ISSUED_AT, Instant.now().getEpochSecond())
-                .claim(JWTClaimNames.EXPIRATION_TIME, Instant.now().plus(1, ChronoUnit.HOURS).getEpochSecond())
+                .claim(JWTClaimNames.EXPIRATION_TIME, Instant.now().plus(2, ChronoUnit.MINUTES).getEpochSecond())
                 .build();
         SignedJWT signedJWT = new SignedJWT(header, claims);
         signedJWT.sign(new RSASSASigner(jwk));
         String assertionToken = signedJWT.serialize();
 
-
         // Send request til tokenX
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        TokenExchangeRequest tokenExchangeRequest = new TokenExchangeRequest();
-        tokenExchangeRequest.setAudience("dev-gcp:arbeidsgiver:altinn-rettigheter-proxy"); // FIXME
-        tokenExchangeRequest.setClient_assertion(assertionToken);
-        tokenExchangeRequest.setClient_assertion_type("urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
-        tokenExchangeRequest.setSubject_token(token.getTokenAsString());
-        tokenExchangeRequest.setSubject_token_type("urn:ietf:params:oauth:token-type:jwt");
-        tokenExchangeRequest.setGrant_type("urn:ietf:params:oauth:grant-type:token-exchange");
-        HttpEntity<TokenExchangeRequest> tokenExchangeRequestHttpEntity = new HttpEntity<TokenExchangeRequest>(tokenExchangeRequest, httpHeaders);
-        TokenExchangeResponse body = restTemplate.postForEntity(authorizationServerMetadata.getTokenEndpointURI(), tokenExchangeRequestHttpEntity, TokenExchangeResponse.class).getBody();
+        MultiValueMap<String, String> parametre = byggParameterMap(assertionToken, token);
+        HttpEntity<MultiValueMap<String, String>> tokenExchangeRequestHttpEntity = new HttpEntity<>(parametre, httpHeaders);
+
+        ResponseEntity<TokenExchangeResponse> responseEntity = restTemplate.postForEntity(
+                authorizationServerMetadata.getTokenEndpointURI(),
+                tokenExchangeRequestHttpEntity,
+                TokenExchangeResponse.class);
+
+        TokenExchangeResponse body = responseEntity
+                .getBody();
         return new JwtToken(body.access_token);
     }
 
-    private static class TokenExchangeResponse {
-        private String access_token;
-        private String issued_token_type;
-        private String token_type;
-        private String expires_in;
+    public MultiValueMap<String, String> byggParameterMap(String assertionToken, JwtToken token) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("audience", "dev-gcp:arbeidsgiver:altinn-rettigheter-proxy"); // FIXME
+        map.add("client_assertion", assertionToken);
+        map.add("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
+        map.add("subject_token", token.getTokenAsString());
+        map.add("subject_token_type", "urn:ietf:params:oauth:token-type:jwt");
+        map.add("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange");
+
+        return map;
     }
 
-    private static class TokenExchangeRequest {
-        private String grant_type;
-        private String client_assertion_type;
-        private String client_assertion;
-        private String subject_token_type;
-        private String subject_token;
-        private String audience;
 
-        public String getGrant_type() {
-            return grant_type;
-        }
-
-        public void setGrant_type(String grant_type) {
-            this.grant_type = grant_type;
-        }
-
-        public String getClient_assertion_type() {
-            return client_assertion_type;
-        }
-
-        public void setClient_assertion_type(String client_assertion_type) {
-            this.client_assertion_type = client_assertion_type;
-        }
-
-        public String getClient_assertion() {
-            return client_assertion;
-        }
-
-        public void setClient_assertion(String client_assertion) {
-            this.client_assertion = client_assertion;
-        }
-
-        public String getSubject_token_type() {
-            return subject_token_type;
-        }
-
-        public void setSubject_token_type(String subject_token_type) {
-            this.subject_token_type = subject_token_type;
-        }
-
-        public String getSubject_token() {
-            return subject_token;
-        }
-
-        public void setSubject_token(String subject_token) {
-            this.subject_token = subject_token;
-        }
-
-        public String getAudience() {
-            return audience;
-        }
-
-        public void setAudience(String audience) {
-            this.audience = audience;
-        }
+    private static class TokenExchangeResponse {
+        public String access_token;
+        public String issued_token_type;
+        public String token_type;
+        public String expires_in;
     }
 
 }
