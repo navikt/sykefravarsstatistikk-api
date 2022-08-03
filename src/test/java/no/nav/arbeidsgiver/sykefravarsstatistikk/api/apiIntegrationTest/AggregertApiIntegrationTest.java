234fbecb21b760d9b8aa3bdc8bb8900d0136f20c
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-08-03 16:24:14 +0200

    Få testen 'hentAgreggertStatistikk' til å kjøre + refactor og nye funksjoner i TestUtils

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/apiIntegrationTest/AggregertApiIntegrationTest.java
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/apiIntegrationTest/AggregertApiIntegrationTest.java
@@ -5,16 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import common.SpringIntegrationTestbase;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestTokenUtil;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Næring;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Orgnr;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.Varighetskategori;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.kvartalsvis.KvartalsvisSykefraværRepository;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Næringskode5Siffer;
 import no.nav.security.mock.oauth2.MockOAuth2Server;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.io.IOException;
@@ -26,9 +23,17 @@ import java.util.List;
 import static com.google.common.net.HttpHeaders.AUTHORIZATION;
 import static java.net.http.HttpClient.newBuilder;
 import static java.net.http.HttpResponse.BodyHandlers.ofString;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestUtils.opprettStatistikkForNæring2Siffer;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestUtils.opprettStatistikkForNæring5Siffer;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestUtils.opprettStatistikkForVirksomhet;
 import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestUtils.oppretteStatistikkForLand;
 import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestUtils.slettAllStatistikkFraDatabase;
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.*;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.PROSENT_SISTE_4_KVARTALER_BRANSJE;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.PROSENT_SISTE_4_KVARTALER_LAND;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.PROSENT_SISTE_4_KVARTALER_NÆRING;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.PROSENT_SISTE_4_KVARTALER_VIRKSOMHET;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.TREND_BRANSJE;
+import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk.Aggregeringstype.TREND_NÆRING;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AggregertApiIntegrationTest extends SpringIntegrationTestbase {
@@ -46,6 +51,7 @@ public class AggregertApiIntegrationTest extends SpringIntegrationTestbase {
     @BeforeEach
     public void setUp() {
         slettAllStatistikkFraDatabase(jdbcTemplate);
+        oppretteStatistikkForLand(jdbcTemplate);
     }
 
     @AfterEach
@@ -57,10 +63,13 @@ public class AggregertApiIntegrationTest extends SpringIntegrationTestbase {
     private String port;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Næringskode5Siffer BARNEHAGER = new Næringskode5Siffer("88911", "Barnehager");
+    private final Næring PRODUKSJON = new Næring("10", "Produksjon av nærings- og nytelsesmidler");
+
 
     @Test
     public void hentAgreggertStatistikk_skal_returnere_403_naar_bruker_ikke_representerer_bedriften()
-          throws Exception {
+            throws Exception {
         String jwtToken = TestTokenUtil.createMockIdportenTokenXToken(mockOAuth2Server);
 
         HttpResponse<String> response = utførAutorisertKall(ORGNR_UNDERENHET_INGEN_TILGANG, jwtToken);
@@ -69,7 +78,10 @@ public class AggregertApiIntegrationTest extends SpringIntegrationTestbase {
 
     @Test
     public void hentAgreggertStatistikk_returnererForventedeTyperForBedriftSomHarAlleTyperData()
-          throws Exception {
+            throws Exception {
+        opprettStatistikkForNæring2Siffer(jdbcTemplate, PRODUKSJON, 2022, 1, 5, 100, 10);
+        opprettStatistikkForNæring2Siffer(jdbcTemplate, PRODUKSJON, 2021, 1, 20, 100, 10);
+        opprettStatistikkForVirksomhet(jdbcTemplate, ORGNR_UNDERENHET, 2022, 1, 5, 100, 10);
         String jwtToken = TestTokenUtil.createMockIdportenTokenXToken(mockOAuth2Server);
 
         HttpResponse<String> response = utførAutorisertKall(ORGNR_UNDERENHET, jwtToken);
@@ -77,116 +89,62 @@ public class AggregertApiIntegrationTest extends SpringIntegrationTestbase {
         JsonNode responseBody = objectMapper.readTree(response.body());
 
         assertThat(responseBody.findValuesAsText("type"))
-              .containsExactlyInAnyOrderElementsOf(
-                    List.of(
-                          PROSENT_SISTE_4_KVARTALER_VIRKSOMHET.toString(),
-                          PROSENT_SISTE_4_KVARTALER_NÆRING.toString(),
-                          PROSENT_SISTE_4_KVARTALER_LAND.toString(),
-                          TREND_NÆRING.toString()
-                    ));
-        /*assertThat(responseBody.findValuesAsText("label"))
-              .containsExactlyInAnyOrderElementsOf(
-                    List.of(
-                          "NAV ARBEID OG YTELSER AVD OSLO",
-                          "Trygdeordninger underlagt offentlig forvaltning",
-                          PROSENT_SISTE_4_KVARTALER_LAND.toString(),
-                          TREND_NÆRING.toString()
-                    ));*/
+                .containsExactlyInAnyOrderElementsOf(
+                        List.of(
+                                PROSENT_SISTE_4_KVARTALER_VIRKSOMHET.toString(),
+                                PROSENT_SISTE_4_KVARTALER_NÆRING.toString(),
+                                PROSENT_SISTE_4_KVARTALER_LAND.toString(),
+                                TREND_NÆRING.toString()
+                        ));
+
+        /* TODO: hva skal vi verifisere her ???
+        assertThat(responseBody.findValuesAsText("label"))
+                .containsExactlyInAnyOrderElementsOf(
+                        List.of(
+                                "NAV ARBEID OG YTELSER AVD OSLO",
+                                "Trygdeordninger underlagt offentlig forvaltning",
+                                PROSENT_SISTE_4_KVARTALER_LAND.toString(),
+                                TREND_NÆRING.toString()
+                        ));
+                        */
     }
-@Test
+
+    @Test
     public void hentAgreggertStatistikk_returnerer_ikke_virksomhet_statistikk_til_bruker_som_mangler_IA_rettigheter()
-          throws Exception {
-    oppretteStatistikkForLand(jdbcTemplate);
-    jdbcTemplate.update(
-            "insert into sykefravar_statistikk_naring5siffer " +
-                    "(naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) "
-                    + "VALUES (:naring_kode, :arstall, :kvartal, :antall_personer, :tapte_dagsverk, :mulige_dagsverk)",
-            parametre(new Næring("88911", "Somatiske spesialsykehjem"), 2022, 1, 10, 5, 100)
-    );
+            throws Exception {
+        opprettStatistikkForNæring5Siffer(jdbcTemplate, BARNEHAGER, 2022, 1, 5, 100, 10);
+        opprettStatistikkForNæring5Siffer(jdbcTemplate, BARNEHAGER, 2021, 1, 1, 100, 10);
 
-    jdbcTemplate.update(
-            "insert into sykefravar_statistikk_naring5siffer " +
-                    "(naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) "
-                    + "VALUES (:naring_kode, :arstall, :kvartal, :antall_personer, :tapte_dagsverk, :mulige_dagsverk)",
-            parametre(new Næring("88911", "Somatiske spesialsykehjem"), 2021, 4, 10, 1, 100)
-    );
-    jdbcTemplate.update(
-            "insert into sykefravar_statistikk_naring5siffer " +
-                    "(naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) "
-                    + "VALUES (:naring_kode, :arstall, :kvartal, :antall_personer, :tapte_dagsverk, :mulige_dagsverk)",
-            parametre(new Næring("88911", "Somatiske spesialsykehjem"), 2021, 3, 10, 1, 100)
-    );
-    jdbcTemplate.update(
-            "insert into sykefravar_statistikk_naring5siffer " +
-                    "(naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) "
-                    + "VALUES (:naring_kode, :arstall, :kvartal, :antall_personer, :tapte_dagsverk, :mulige_dagsverk)",
-            parametre(new Næring("88911", "Somatiske spesialsykehjem"), 2021, 2, 10, 1, 100)
-    );
-    jdbcTemplate.update(
-            "insert into sykefravar_statistikk_naring5siffer " +
-                    "(naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) "
-                    + "VALUES (:naring_kode, :arstall, :kvartal, :antall_personer, :tapte_dagsverk, :mulige_dagsverk)",
-            parametre(new Næring("88911", "Somatiske spesialsykehjem"), 2021, 1, 10, 1, 100)
-    );
-
-    String jwtToken = TestTokenUtil.createMockIdportenTokenXToken(mockOAuth2Server);
+        String jwtToken = TestTokenUtil.createMockIdportenTokenXToken(mockOAuth2Server);
 
         HttpResponse<String> response = utførAutorisertKall(ORGNR_UNDERENHET_UTEN_IA_RETTIGHETER, jwtToken);
         assertThat(response.statusCode()).isEqualTo(200);
         JsonNode responseBody = objectMapper.readTree(response.body());
 
         assertThat(responseBody.findValuesAsText("type"))
-              .containsExactlyInAnyOrderElementsOf(
-                    List.of(
-                          PROSENT_SISTE_4_KVARTALER_BRANSJE.toString(),
-                          PROSENT_SISTE_4_KVARTALER_LAND.toString(),
-                          TREND_BRANSJE.toString()
-                    ));
-
+                .containsExactlyInAnyOrderElementsOf(
+                        List.of(
+                                PROSENT_SISTE_4_KVARTALER_BRANSJE.toString(),
+                                PROSENT_SISTE_4_KVARTALER_LAND.toString(),
+                                TREND_BRANSJE.toString()
+                        ));
     }
 
     private HttpResponse<String> utførAutorisertKall(String orgnr, String jwtToken)
-          throws IOException, InterruptedException {
+            throws IOException, InterruptedException {
         return newBuilder().build().send(
-              HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:" + port + "/sykefravarsstatistikk-api/"
-                          + orgnr +
-                          "/sykefravarshistorikk/aggregert/siste")
-                    )
-                    .header(
-                          AUTHORIZATION,
-                          "Bearer " + jwtToken
-                    )
-                    .GET()
-                    .build(),
-              ofString()
+                HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:" + port + "/sykefravarsstatistikk-api/"
+                                + orgnr +
+                                "/sykefravarshistorikk/aggregert/siste")
+                        )
+                        .header(
+                                AUTHORIZATION,
+                                "Bearer " + jwtToken
+                        )
+                        .GET()
+                        .build(),
+                ofString()
         );
-    }
-
-    private MapSqlParameterSource parametre(int årstall, int kvartal, int antallPersoner, int tapteDagsverk, int muligeDagsverk) {
-        return new MapSqlParameterSource()
-                .addValue("arstall", årstall)
-                .addValue("kvartal", kvartal)
-                .addValue("antall_personer", antallPersoner)
-                .addValue("tapte_dagsverk", tapteDagsverk)
-                .addValue("mulige_dagsverk", muligeDagsverk);
-    }
-    private MapSqlParameterSource parametre(Næring næring, int årstall, int kvartal, int antallPersoner, int tapteDagsverk, int muligeDagsverk) {
-        return parametre(årstall, kvartal, antallPersoner, tapteDagsverk, muligeDagsverk)
-                .addValue("naring_kode", næring.getKode());
-    }
-
-    private MapSqlParameterSource parametre(
-            Orgnr orgnr,
-            int årstall,
-            int kvartal,
-            int antallPersoner,
-            int tapteDagsverk,
-            int muligeDagsverk,
-            Varighetskategori varighet
-    ) {
-        return parametre(årstall, kvartal, antallPersoner, tapteDagsverk, muligeDagsverk)
-                .addValue("orgnr", orgnr.getVerdi())
-                .addValue("varighet", varighet.kode);
     }
 }
