145ac3f19d8d18a8fcf7745d0c9987495618fa18
runarskagestad <runar.skagestad@nav.no>
2022-08-05 15:54:13 +0200

    Skrev tester for kalkulerSykefraværsprosent, og justerte på bakgrunn av dem antall sifre i utregningen

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/StatistikkUtils.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/StatistikkUtils.java
@@ -7,15 +7,16 @@ import org.jetbrains.annotations.NotNull;
 public class StatistikkUtils {
 
     /**
-     * Ment å være en felles source of trouth for kalkulering av sykefraværsprosent basert på tapte
-     * dagsverk og mulige dagsverk. Denne metoden bør altså ikke dupliseres, men refaktorer gjerne
-     * eksisterende metoder slik at de også bruker denne.
+     * Source of trouth for kalkulering av sykefraværsprosent basert på tapte dagsverk og mulige
+     * dagsverk. ArithmeticException kastes dersom muligeDagsverk er 0.
      */
     public static BigDecimal kalkulerSykefraværsprosent(
-          @NotNull BigDecimal tapteDagsverk,
-          @NotNull BigDecimal muligeDagsverk) {
-        final int GJELDENDE_SIFFER = 2;
-        return tapteDagsverk.divide(muligeDagsverk, GJELDENDE_SIFFER, RoundingMode.HALF_UP)
-              .multiply(new BigDecimal(100));
+          @NotNull BigDecimal tapteDagsverk, @NotNull BigDecimal muligeDagsverk) {
+        final int ANTALL_SIFRE_I_UTREGNING = 3;
+        final int ANTALL_SIFRE_I_RESULTAT = 1;
+        return tapteDagsverk.divide(
+                    muligeDagsverk, ANTALL_SIFRE_I_UTREGNING, RoundingMode.HALF_UP)
+              .multiply(new BigDecimal(100))
+              .setScale(ANTALL_SIFRE_I_RESULTAT, RoundingMode.HALF_UP);
     }
 }
