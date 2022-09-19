a9fcabd5a53c90452359c6d0a530d6e9a166dd1f
runarskagestad <runar.skagestad@nav.no>
2022-09-19 13:05:07 +0200

    La til testdata for næring5siffer

---
+++ src/main/resources/db/test-lokaldb-data/V4.01__naring5siffer_statistikk_sykefravar.sql
@@ -0,0 +1,16 @@
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2019, 1, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2019, 2, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2019, 3, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2019, 4, 10, 144324.778468, 2562076.941924);
+
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2020, 1, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2020, 2, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2020, 3, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2020, 4, 10, 144324.778468, 2562076.941924);
+
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2021, 1, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2021, 2, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2021, 3, 10, 144324.778468, 2562076.941924);
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2021, 4, 10, 144324.778468, 2562076.941924);
+
+insert into sykefravar_statistikk_naring5siffer (naring_kode, arstall, kvartal, antall_personer, tapte_dagsverk, mulige_dagsverk) values ('10062', 2022, 1, 10, 144324.778468, 2562076.941924);
