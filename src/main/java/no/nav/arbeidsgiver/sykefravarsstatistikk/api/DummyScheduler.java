4fcf37e8bbece9f416007949e5b6fb2e42589ab7
runarskagestad <runar.skagestad@nav.no>
2022-10-07 13:52:13 +0200

    Endrer til 15 min kjøretid på dummy-jobb

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/DummyScheduler.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/DummyScheduler.java
@@ -2,15 +2,7 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import lombok.extern.slf4j.Slf4j;
-import net.javacrumbs.shedlock.core.LockConfiguration;
-import net.javacrumbs.shedlock.core.LockingTaskExecutor;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.importering.autoimport.Importeringstatus;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.importering.autoimport.SykefraværsstatistikkImporteringService;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +16,7 @@ public class DummyScheduler {
     this.counter = registry.counter("dummy_counter");
   }
 
-  @Scheduled(cron = "*/30 * * * * ?")
+  @Scheduled(cron = "* */15 * * * ?")
   public void scheduledImportering() {
     log.info("Kjører dummy-jobb, og inkrementerer counter ");
     log.info("Counter er nå (pre-increment): {}", counter.count());
