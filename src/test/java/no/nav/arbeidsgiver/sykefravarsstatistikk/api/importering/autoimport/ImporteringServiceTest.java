3c6779701e99b5f405a510a99df627271c7561a6
runarskagestad <runar.skagestad@nav.no>
2022-11-28 12:45:33 +0100

    Fikser testkompilering

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/importering/autoimport/ImporteringServiceTest.java
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/importering/autoimport/ImporteringServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.List;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.ÅrstallOgKvartal;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.env.Environment;
@@ -12,14 +13,14 @@ public class ImporteringServiceTest {
 
   Environment environment;
   SykefraværsstatistikkImporteringService importeringService = new SykefraværsstatistikkImporteringService(
-      null, null, null, true, null, environment);
+      null, null, null, true, null);
 
   @Test
   public void kanImportStartes__returnerer_TRUE_dersom_alle_årstall_og_kvartal_er_like_OG_sykefraværsstatistikk_ligger_ett_kvartal_bak_Dvh() {
     assertFalse(
         importeringService.kanImportStartes(
-            Arrays.asList(new ÅrstallOgKvartal(2019, 3)),
-            Arrays.asList(new ÅrstallOgKvartal(2020, 1))
+            List.of(new ÅrstallOgKvartal(2019, 3)),
+            List.of(new ÅrstallOgKvartal(2020, 1))
         )
     );
 
