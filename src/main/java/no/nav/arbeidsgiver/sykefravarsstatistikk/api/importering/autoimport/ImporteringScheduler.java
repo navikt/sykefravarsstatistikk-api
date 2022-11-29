3575c56b6c766c01407c64aaf5ae716a9c8e090a
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2022-11-29 15:12:10 +0100

    reverter til importering én gang i døgnet

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/importering/autoimport/ImporteringScheduler.java
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/importering/autoimport/ImporteringScheduler.java
@@ -26,7 +26,7 @@ public class ImporteringScheduler {
     this.counter = registry.counter("sykefravarstatistikk_vellykket_import");
   }
 
-  @Scheduled(cron = "0 */20 * * * ?")
+  @Scheduled(cron = "0 5 8 * * ?")
   public void scheduledImportering() {
     Duration lockAtMostFor = Duration.of(10, ChronoUnit.MINUTES);
     Duration lockAtLeastFor = Duration.of(1, ChronoUnit.MINUTES);
