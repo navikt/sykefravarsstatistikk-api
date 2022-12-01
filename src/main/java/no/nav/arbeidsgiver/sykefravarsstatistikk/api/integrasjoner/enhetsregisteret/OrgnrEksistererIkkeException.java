f65a5cce40d85ae2ea57f1cd9b3c3f5e6feb5d67
Tharald <tharaldyo@users.noreply.github.com>
2022-12-01 14:43:48 +0100

    lage ny exception med tilhørende håndtering for når orgnr ikke finnes… (#332)
    håndtere feil fra enhetsregisteret forårsaket av at orgnr ikke eksisterer

---
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/enhetsregisteret/OrgnrEksistererIkkeException.java
@@ -0,0 +1,8 @@
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.integrasjoner.enhetsregisteret;
+
+public class OrgnrEksistererIkkeException extends EnhetsregisteretException {
+
+  OrgnrEksistererIkkeException(String msg) {
+    super(msg);
+  }
+}
