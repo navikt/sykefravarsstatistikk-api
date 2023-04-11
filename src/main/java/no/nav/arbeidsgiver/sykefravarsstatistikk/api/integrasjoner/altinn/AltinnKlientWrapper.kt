e5943b92c60c66ec0f176387b1721c533168516d
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-04-11 15:50:33 +0200

    Injekser AltinnKlient som bønne

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/altinn/AltinnKlientWrapper.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/altinn/AltinnKlientWrapper.kt
@@ -1,13 +1,11 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.integrasjoner.altinn
 
 import lombok.extern.slf4j.Slf4j
-import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnConfig
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnrettigheterProxyKlient
-import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnrettigheterProxyKlientConfig
-import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.ProxyConfig
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.model.*
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.Fnr
 import no.nav.security.token.support.core.jwt.JwtToken
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.stream.Collectors
@@ -15,22 +13,11 @@ import java.util.stream.Collectors
 @Slf4j
 @Component
 open class AltinnKlientWrapper(
-    @Value("\${altinn.proxy.url}") altinnProxyUrl: String?,
-    @Value("\${altinn.url}") altinnUrl: String?,
-    @Value("\${altinn.apigw.apikey}") altinnAPIGWApikey: String?,
-    @Value("\${altinn.apikey}") altinnApikey: String?,
+    @Autowired val klient: AltinnrettigheterProxyKlient,
     @param:Value("\${altinn.iaweb.service.code}") private val serviceCode: String,
     @param:Value("\${altinn.iaweb.service.edition}") private val serviceEdition: String
 ) {
-    private val klient: AltinnrettigheterProxyKlient
 
-    init {
-        val config = AltinnrettigheterProxyKlientConfig(
-            ProxyConfig("sykefraværsstatistikk", altinnProxyUrl!!),
-            AltinnConfig(altinnUrl!!, altinnApikey!!, altinnAPIGWApikey!!)
-        )
-        klient = AltinnrettigheterProxyKlient(config)
-    }
 
     open fun hentOrgnumreDerBrukerHarEnkeltrettighetTilIAWeb(
         idToken: JwtToken, fnr: Fnr
