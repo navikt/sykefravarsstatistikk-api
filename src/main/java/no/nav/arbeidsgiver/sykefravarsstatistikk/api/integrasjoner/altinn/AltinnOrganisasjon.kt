f387d1fa155ade2af092bf2e04948cfbe2e8ad3f
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-04-12 10:04:26 +0200

    Refactor AltinnOrganisasjon

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/altinn/AltinnOrganisasjon.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/integrasjoner/altinn/AltinnOrganisasjon.kt
@@ -2,10 +2,8 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.integrasjoner.altinn
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import lombok.Data
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Data
 data class AltinnOrganisasjon(
     @field:JsonProperty("Name") val name: String?,
     @field:JsonProperty("Type") val type: String?,
