d0bbb404f7f5f45d04bb8a4db2479a2d07bed85b
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-05-30 14:32:44 +0200

    Add comment

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/ProxyWebClient.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/config/ProxyWebClient.kt
@@ -4,6 +4,9 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 
+/**
+ * A WebClient that uses a proxy for onprem
+ */
 open class ProxyWebClient private constructor(webClient: WebClient) : WebClient by webClient {
     constructor(webClientBuilder: WebClient.Builder) : this(
         webClientBuilder.clientConnector(
