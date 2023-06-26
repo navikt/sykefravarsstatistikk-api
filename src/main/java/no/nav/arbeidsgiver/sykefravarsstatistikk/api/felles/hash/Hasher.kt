352a843160c8f7ad027a3f6ded383c406bffd117
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-06-26 14:15:21 +0200

    Revert "Add hasher"
    This reverts commit 68bdf3398c57eafc8f2c34ac49a0b237c0e546b9.

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/felles/hash/Hasher.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.hash
-
-interface Hasher {
-    fun hash(data: String, salt: ByteArray): ByteArray
-    fun generateRandomSalt(): ByteArray
-}
