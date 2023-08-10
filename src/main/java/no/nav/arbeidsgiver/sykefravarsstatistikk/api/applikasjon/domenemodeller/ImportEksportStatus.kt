324b11e0890160f17f53fdfbe84a466c2ee9c1b6
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-08-10 15:30:20 +0200

    Legg til kommentar

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/domenemodeller/ImportEksportStatus.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/domenemodeller/ImportEksportStatus.kt
@@ -1,5 +1,9 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller
 
+/**
+ * Viser status på alle stegene som gjøres ved automatisk
+ * import og eksport av sykefraværsstatistikk.
+ */
 interface ImportEksportStatus {
     val årstallOgKvartal: ÅrstallOgKvartal
     val importertStatistikk: Boolean
