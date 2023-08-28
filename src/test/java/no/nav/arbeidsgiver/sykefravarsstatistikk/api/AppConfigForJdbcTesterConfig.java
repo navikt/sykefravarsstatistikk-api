c3ce7fbfb793030c5b43864b46cb8a0e40d100cb
Runar Skagestad <runska@gmail.com>
2023-08-28 15:56:32 +0200

    Fjern ubrukt Controller og Service

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/AppConfigForJdbcTesterConfig.java
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/AppConfigForJdbcTesterConfig.java
@@ -1,26 +1,25 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api;
 
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.PostImporteringService;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.TilgangskontrollService;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.aggregering.AggregertStatistikkService;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.eksportering.EksporteringMetadataVirksomhetService;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.eksportering.EksporteringPerStatistikkKategoriService;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.eksportering.EksporteringService;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.PrometheusMetrics;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.ImporteringController;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.cron.ImporteringScheduler;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.PostImporteringService;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.importering.SykefraværsstatistikkImporteringService;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.MockServer;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.utils.TilgangskontrollUtils;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.config.WebClientConfiguration;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.EksporteringController;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.healthcheck.HealthcheckController;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.MockServer;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.PrometheusMetrics;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.altinn.AltinnClient;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.EksporteringController;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.ImporteringController;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.OrganisasjonerController;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.SykefraværshistorikkController;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.healthcheck.HealthcheckController;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.cron.ImporteringScheduler;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.enhetsregisteret.EnhetsregisteretClient;
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.kafka.KafkaClient;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.OrganisasjonerController;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.OffentligSykefraværshistorikkController;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.api.SykefraværshistorikkController;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.aggregering.AggregertStatistikkService;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.TilgangskontrollService;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.utils.TilgangskontrollUtils;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -54,7 +53,6 @@ import org.springframework.data.jdbc.repository.config.JdbcRepositoryConfigExten
             SykefraværshistorikkController.class,
             OrganisasjonerController.class,
             TilgangskontrollService.class,
-            OffentligSykefraværshistorikkController.class,
             AggregertStatistikkService.class,
             EksporteringMetadataVirksomhetService.class,
             HealthcheckController.class,
