cb116c05d8b72035775310dcae64afe246867876
Runar Skagestad <runska@gmail.com>
2023-09-21 22:59:37 +0200

    Konstanter -> kt

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/config/Konstanter.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/config/Konstanter.kt
@@ -1,7 +1,5 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.config;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.config
 
-public class Konstanter {
-  public static final int MIN_ANTALL_PERS_FOR_AT_STATISTIKKEN_IKKE_ER_PERSONOPPLYSNINGER = 5;
-
-  public static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-ID";
+object Konstanter {
+    const val MIN_ANTALL_PERS_FOR_AT_STATISTIKKEN_IKKE_ER_PERSONOPPLYSNINGER = 5
 }
