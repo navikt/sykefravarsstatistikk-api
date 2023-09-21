9b35623fcd3cc52bd6627051ac4f49351678b303
Runar Skagestad <runska@gmail.com>
2023-09-21 23:29:04 +0200

    PubliseringsdatoerRepository -> Kt

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/database/PubliseringsdatoerRepository.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/database/PubliseringsdatoerRepository.kt
@@ -1,61 +1,52 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database
 
-import io.vavr.control.Option;
-import io.vavr.control.Try;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import lombok.extern.slf4j.Slf4j;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ImporttidspunktDto;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.PubliseringsdatoDbDto;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ÅrstallOgKvartal;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Component;
+import io.vavr.control.Option
+import io.vavr.control.Try
+import lombok.extern.slf4j.Slf4j
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ImporttidspunktDto
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.PubliseringsdatoDbDto
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ÅrstallOgKvartal
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Component
+import java.sql.ResultSet
+import java.time.LocalDateTime
 
 @Slf4j
 @Component
-public class PubliseringsdatoerRepository {
-
-  private final NamedParameterJdbcTemplate jdbcTemplate;
-
-  public PubliseringsdatoerRepository(
-      @Qualifier("sykefravarsstatistikkJdbcTemplate")
-          NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-    this.jdbcTemplate = namedParameterJdbcTemplate;
-  }
-
-  public List<PubliseringsdatoDbDto> hentPubliseringsdatoer() {
-    try {
-      return jdbcTemplate.query(
-          "select * from publiseringsdatoer",
-          new HashMap<>(),
-          (rs, rowNum) ->
-              new PubliseringsdatoDbDto(
-                  rs.getInt("rapport_periode"),
-                  rs.getDate("offentlig_dato"),
-                  rs.getDate("oppdatert_i_dvh"),
-                  rs.getString("aktivitet")));
-    } catch (EmptyResultDataAccessException e) {
-      log.error("Fant ikke publiseringsdatoer i databasen, returnerer tom liste");
-      return Collections.emptyList();
+class PubliseringsdatoerRepository(
+    @param:Qualifier("sykefravarsstatistikkJdbcTemplate") private val jdbcTemplate: NamedParameterJdbcTemplate
+) {
+    private val log = LoggerFactory.getLogger(this::class.java)
+    fun hentPubliseringsdatoer(): List<PubliseringsdatoDbDto> {
+        return try {
+            jdbcTemplate.query(
+                "select * from publiseringsdatoer",
+                HashMap<String, Any?>()
+            ) { rs: ResultSet, _: Int ->
+                PubliseringsdatoDbDto(
+                    rs.getInt("rapport_periode"),
+                    rs.getDate("offentlig_dato"),
+                    rs.getDate("oppdatert_i_dvh"),
+                    rs.getString("aktivitet")
+                )
+            }
+        } catch (e: EmptyResultDataAccessException) {
+            log.error("Fant ikke publiseringsdatoer i databasen, returnerer tom liste")
+            emptyList()
+        }
     }
-  }
 
-  public void oppdaterPubliseringsdatoer(List<PubliseringsdatoDbDto> data) {
-    int antallRadetSlettet =
-        jdbcTemplate.update("delete from publiseringsdatoer", new MapSqlParameterSource());
-    log.info("Antall rader slettet fra 'publiseringsdatoer': " + antallRadetSlettet);
-
-    int antallRaderSattInn =
-        data.stream()
-            .mapToInt(
-                publiseringsdato ->
-                    jdbcTemplate.update(
-                        "insert into publiseringsdatoer "
+    fun oppdaterPubliseringsdatoer(data: List<PubliseringsdatoDbDto>) {
+        val antallRadetSlettet = jdbcTemplate.update("delete from publiseringsdatoer", MapSqlParameterSource())
+        log.info("Antall rader slettet fra 'publiseringsdatoer': $antallRadetSlettet")
+        val antallRaderSattInn = data.stream()
+            .mapToInt { (rapportPeriode, offentligDato, oppdatertDato, aktivitet): PubliseringsdatoDbDto ->
+                jdbcTemplate.update(
+                    "insert into publiseringsdatoer "
                             + "(rapport_periode, "
                             + "offentlig_dato, "
                             + "oppdatert_i_dvh, "
@@ -65,43 +56,46 @@ public class PubliseringsdatoerRepository {
                             + ":offentlig_dato, "
                             + ":oppdatert_i_dvh, "
                             + ":aktivitet)",
-                        new MapSqlParameterSource()
-                            .addValue("rapport_periode", publiseringsdato.getRapportPeriode())
-                            .addValue("offentlig_dato", publiseringsdato.getOffentligDato())
-                            .addValue("oppdatert_i_dvh", publiseringsdato.getOppdatertDato())
-                            .addValue("aktivitet", publiseringsdato.getAktivitet())))
-            .sum();
-    log.info("Antall rader satt inn i 'publiseringsdatoer': " + antallRaderSattInn);
-  }
+                    MapSqlParameterSource()
+                        .addValue("rapport_periode", rapportPeriode)
+                        .addValue("offentlig_dato", offentligDato)
+                        .addValue("oppdatert_i_dvh", oppdatertDato)
+                        .addValue("aktivitet", aktivitet)
+                )
+            }
+            .sum()
+        log.info("Antall rader satt inn i 'publiseringsdatoer': $antallRaderSattInn")
+    }
 
-  public void oppdaterSisteImporttidspunkt(ÅrstallOgKvartal årstallOgKvartal) {
-    log.info("Oppdaterert tidspunkt for import av sykefraværstatistikk for " + årstallOgKvartal);
-    int updatedRows =
-        jdbcTemplate.update(
+    fun oppdaterSisteImporttidspunkt(årstallOgKvartal: ÅrstallOgKvartal) {
+        log.info("Oppdaterert tidspunkt for import av sykefraværstatistikk for $årstallOgKvartal")
+        val updatedRows = jdbcTemplate.update(
             "insert into importtidspunkt (aarstall, kvartal, importert) values "
-                + "(:aarstall, :kvartal, :importert)",
-            new MapSqlParameterSource()
-                .addValue("aarstall", årstallOgKvartal.getÅrstall())
-                .addValue("kvartal", årstallOgKvartal.getKvartal())
-                .addValue("importert", LocalDateTime.now()));
-    log.info("Opprettet " + updatedRows + " rader");
-  }
+                    + "(:aarstall, :kvartal, :importert)",
+            MapSqlParameterSource()
+                .addValue("aarstall", årstallOgKvartal.årstall)
+                .addValue("kvartal", årstallOgKvartal.kvartal)
+                .addValue("importert", LocalDateTime.now())
+        )
+        log.info("Opprettet $updatedRows rader")
+    }
 
-  public Option<ImporttidspunktDto> hentSisteImporttidspunkt() {
-    return Try.of(this::hentSisteImporttidspunktFraDb)
-        .onFailure(feil -> log.error("Klarte ikke hente ut siste importtidspunkt: " + feil))
-        .toOption();
-  }
+    fun hentSisteImporttidspunkt(): Option<ImporttidspunktDto> {
+        return Try.of { hentSisteImporttidspunktFraDb() }
+            .onFailure { feil: Throwable -> log.error("Klarte ikke hente ut siste importtidspunkt: $feil") }
+            .toOption()
+    }
 
-  private ImporttidspunktDto hentSisteImporttidspunktFraDb() {
-    return jdbcTemplate
-        .query(
-            "select * from importtidspunkt order by importert desc " + "fetch first 1 rows only",
-            new HashMap<>(),
-            (rs, rowNum) ->
-                new ImporttidspunktDto(
+    private fun hentSisteImporttidspunktFraDb(): ImporttidspunktDto {
+        return jdbcTemplate
+            .query(
+                "select * from importtidspunkt order by importert desc " + "fetch first 1 rows only",
+                HashMap<String, Any?>()
+            ) { rs: ResultSet, _: Int ->
+                ImporttidspunktDto(
                     rs.getTimestamp("importert"),
-                    new ÅrstallOgKvartal(rs.getInt("aarstall"), rs.getInt("kvartal"))))
-        .get(0);
-  }
+                    ÅrstallOgKvartal(rs.getInt("aarstall"), rs.getInt("kvartal"))
+                )
+            }[0]
+    }
 }
