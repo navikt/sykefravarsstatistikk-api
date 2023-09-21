a6727f52dc7b38183b9bb1ff4965265eedee422b
Runar Skagestad <runska@gmail.com>
2023-09-21 23:26:52 +0200

    GraderingRepository -> Kt

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/database/GraderingRepository.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/database/GraderingRepository.kt
@@ -1,153 +1,140 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database
 
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.*;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.Bransje;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.Bransjeprogram;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Component;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.*;
-
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Statistikkategori.BRANSJE;
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Statistikkategori.NÆRING;
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.datavarehus.DatavarehusRepository.RECTYPE_FOR_VIRKSOMHET;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.*
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.Bransje
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.Bransjeprogram.finnBransje
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.datavarehus.DatavarehusRepository
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Component
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.util.*
 
 @Component
-public class GraderingRepository {
-
-  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-  public GraderingRepository(
-      @Qualifier("sykefravarsstatistikkJdbcTemplate")
-          NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-  }
-
-  public List<VirksomhetMetadataMedNæringskode> hentVirksomhetMetadataNæringskode5siffer(
-      ÅrstallOgKvartal årstallOgKvartal) {
-    try {
-      return namedParameterJdbcTemplate.query(
-          "select arstall, kvartal, orgnr, naring, naring_kode"
-              + " from sykefravar_statistikk_virksomhet_med_gradering "
-              + " where "
-              + " arstall = :årstall "
-              + " and kvartal = :kvartal "
-              + " group by arstall, kvartal, orgnr, naring, naring_kode"
-              + " order by arstall, kvartal, orgnr, naring, naring_kode",
-          new MapSqlParameterSource()
-              .addValue("årstall", årstallOgKvartal.getÅrstall())
-              .addValue("kvartal", årstallOgKvartal.getKvartal()),
-          (rs, rowNum) ->
-              new VirksomhetMetadataMedNæringskode(
-                  new Orgnr(rs.getString("orgnr")),
-                  new ÅrstallOgKvartal(rs.getInt("arstall"), rs.getInt("kvartal")),
-                  new Næringskode(rs.getString("naring_kode"))
-              ));
-    } catch (EmptyResultDataAccessException e) {
-      return Collections.emptyList();
+class GraderingRepository(
+    @param:Qualifier("sykefravarsstatistikkJdbcTemplate") private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate
+) {
+    fun hentVirksomhetMetadataNæringskode5siffer(
+        årstallOgKvartal: ÅrstallOgKvartal
+    ): List<VirksomhetMetadataMedNæringskode> {
+        return try {
+            namedParameterJdbcTemplate.query(
+                "select arstall, kvartal, orgnr, naring, naring_kode"
+                        + " from sykefravar_statistikk_virksomhet_med_gradering "
+                        + " where "
+                        + " arstall = :årstall "
+                        + " and kvartal = :kvartal "
+                        + " group by arstall, kvartal, orgnr, naring, naring_kode"
+                        + " order by arstall, kvartal, orgnr, naring, naring_kode",
+                MapSqlParameterSource()
+                    .addValue("årstall", årstallOgKvartal.årstall)
+                    .addValue("kvartal", årstallOgKvartal.kvartal)
+            ) { rs: ResultSet, _: Int ->
+                VirksomhetMetadataMedNæringskode(
+                    Orgnr(rs.getString("orgnr")),
+                    ÅrstallOgKvartal(rs.getInt("arstall"), rs.getInt("kvartal")),
+                    Næringskode(rs.getString("naring_kode"))
+                )
+            }
+        } catch (e: EmptyResultDataAccessException) {
+            emptyList()
+        }
     }
-  }
 
-  public List<UmaskertSykefraværForEttKvartal> hentSykefraværMedGradering(Virksomhet virksomhet) {
-    try {
-      return namedParameterJdbcTemplate.query(
-          "select arstall, kvartal,"
-              + " sum(tapte_dagsverk_gradert_sykemelding) as "
-              + "sum_tapte_dagsverk_gradert_sykemelding, "
-              + " sum(tapte_dagsverk) as sum_tapte_dagsverk, "
-              + " sum(antall_personer) as sum_antall_personer "
-              + " from sykefravar_statistikk_virksomhet_med_gradering "
-              + " where "
-              + " orgnr = :orgnr "
-              + " and rectype = :rectype "
-              + " group by arstall, kvartal"
-              + " order by arstall, kvartal",
-          new MapSqlParameterSource()
-              .addValue("orgnr", virksomhet.getOrgnr().getVerdi())
-              .addValue("rectype", RECTYPE_FOR_VIRKSOMHET),
-          (rs, rowNum) -> mapTilUmaskertSykefraværForEttKvartal(rs));
-    } catch (EmptyResultDataAccessException e) {
-      return Collections.emptyList();
+    fun hentSykefraværMedGradering(virksomhet: Virksomhet): List<UmaskertSykefraværForEttKvartal> {
+        return try {
+            namedParameterJdbcTemplate.query(
+                "select arstall, kvartal,"
+                        + " sum(tapte_dagsverk_gradert_sykemelding) as "
+                        + "sum_tapte_dagsverk_gradert_sykemelding, "
+                        + " sum(tapte_dagsverk) as sum_tapte_dagsverk, "
+                        + " sum(antall_personer) as sum_antall_personer "
+                        + " from sykefravar_statistikk_virksomhet_med_gradering "
+                        + " where "
+                        + " orgnr = :orgnr "
+                        + " and rectype = :rectype "
+                        + " group by arstall, kvartal"
+                        + " order by arstall, kvartal",
+                MapSqlParameterSource()
+                    .addValue("orgnr", virksomhet.orgnr.verdi)
+                    .addValue("rectype", DatavarehusRepository.RECTYPE_FOR_VIRKSOMHET)
+            ) { rs: ResultSet, _: Int -> mapTilUmaskertSykefraværForEttKvartal(rs) }
+        } catch (e: EmptyResultDataAccessException) {
+            emptyList()
+        }
     }
-  }
 
-  public Sykefraværsdata hentGradertSykefraværAlleKategorier(@NotNull Virksomhet virksomhet) {
-
-    Næring næring = virksomhet.getNæringskode().getNæring();
-    Optional<Bransje> maybeBransje = Bransjeprogram.finnBransje(virksomhet.getNæringskode());
-
-    Map<Statistikkategori, List<UmaskertSykefraværForEttKvartal>> data = new HashMap<>();
-
-    data.put(Statistikkategori.VIRKSOMHET, hentSykefraværMedGradering(virksomhet));
-
-    if (maybeBransje.isEmpty()) {
-      data.put(NÆRING, hentSykefraværMedGradering(næring));
-    } else if (maybeBransje.get().erDefinertPåFemsiffernivå()) {
-      data.put(BRANSJE, hentSykefraværMedGradering(maybeBransje.get()));
-    } else {
-      data.put(BRANSJE, hentSykefraværMedGradering(næring));
+    fun hentGradertSykefraværAlleKategorier(virksomhet: Virksomhet): Sykefraværsdata {
+        val næring = virksomhet.næringskode.næring
+        val maybeBransje = finnBransje(virksomhet.næringskode)
+        val data: MutableMap<Statistikkategori, List<UmaskertSykefraværForEttKvartal>> = EnumMap(Statistikkategori::class.java)
+        data[Statistikkategori.VIRKSOMHET] = hentSykefraværMedGradering(virksomhet)
+        if (maybeBransje.isEmpty) {
+            data[Statistikkategori.NÆRING] = hentSykefraværMedGradering(næring)
+        } else if (maybeBransje.get().erDefinertPåFemsiffernivå()) {
+            data[Statistikkategori.BRANSJE] = hentSykefraværMedGradering(maybeBransje.get())
+        } else {
+            data[Statistikkategori.BRANSJE] = hentSykefraværMedGradering(næring)
+        }
+        return Sykefraværsdata(data)
     }
-    return new Sykefraværsdata(data);
-  }
 
-  public List<UmaskertSykefraværForEttKvartal> hentSykefraværMedGradering(Næring næring) {
-    try {
-      return namedParameterJdbcTemplate.query(
-          "select arstall, kvartal,"
-              + " sum(tapte_dagsverk_gradert_sykemelding) as "
-              + "sum_tapte_dagsverk_gradert_sykemelding, "
-              + " sum(tapte_dagsverk) as sum_tapte_dagsverk, "
-              + " sum(antall_personer) as sum_antall_personer "
-              + " from sykefravar_statistikk_virksomhet_med_gradering "
-              + " where "
-              + " naring = :naring "
-              + " and rectype = :rectype "
-              + " group by arstall, kvartal"
-              + " order by arstall, kvartal",
-          new MapSqlParameterSource()
-              .addValue("naring", næring.getTosifferIdentifikator())
-              .addValue("rectype", RECTYPE_FOR_VIRKSOMHET),
-          (rs, rowNum) -> mapTilUmaskertSykefraværForEttKvartal(rs));
-    } catch (EmptyResultDataAccessException e) {
-      return Collections.emptyList();
+    fun hentSykefraværMedGradering(næring: Næring): List<UmaskertSykefraværForEttKvartal> {
+        return try {
+            namedParameterJdbcTemplate.query(
+                "select arstall, kvartal,"
+                        + " sum(tapte_dagsverk_gradert_sykemelding) as "
+                        + "sum_tapte_dagsverk_gradert_sykemelding, "
+                        + " sum(tapte_dagsverk) as sum_tapte_dagsverk, "
+                        + " sum(antall_personer) as sum_antall_personer "
+                        + " from sykefravar_statistikk_virksomhet_med_gradering "
+                        + " where "
+                        + " naring = :naring "
+                        + " and rectype = :rectype "
+                        + " group by arstall, kvartal"
+                        + " order by arstall, kvartal",
+                MapSqlParameterSource()
+                    .addValue("naring", næring.tosifferIdentifikator)
+                    .addValue("rectype", DatavarehusRepository.RECTYPE_FOR_VIRKSOMHET)
+            ) { rs: ResultSet, _: Int -> mapTilUmaskertSykefraværForEttKvartal(rs) }
+        } catch (e: EmptyResultDataAccessException) {
+            emptyList()
+        }
     }
-  }
 
-  public List<UmaskertSykefraværForEttKvartal> hentSykefraværMedGradering(Bransje bransje) {
-    try {
-      return namedParameterJdbcTemplate.query(
-          "select arstall, kvartal,"
-              + " sum(tapte_dagsverk_gradert_sykemelding) as "
-              + "sum_tapte_dagsverk_gradert_sykemelding, "
-              + " sum(tapte_dagsverk) as sum_tapte_dagsverk, "
-              + " sum(antall_personer) as sum_antall_personer "
-              + " from sykefravar_statistikk_virksomhet_med_gradering "
-              + " where "
-              + " naring_kode in (:naringKoder) "
-              + " and rectype = :rectype "
-              + " group by arstall, kvartal"
-              + " order by arstall, kvartal",
-          new MapSqlParameterSource()
-              .addValue("naringKoder", bransje.getIdentifikatorer())
-              .addValue("rectype", RECTYPE_FOR_VIRKSOMHET),
-          (rs, rowNum) -> mapTilUmaskertSykefraværForEttKvartal(rs));
-    } catch (EmptyResultDataAccessException e) {
-      return Collections.emptyList();
+    fun hentSykefraværMedGradering(bransje: Bransje): List<UmaskertSykefraværForEttKvartal> {
+        return try {
+            namedParameterJdbcTemplate.query(
+                "select arstall, kvartal,"
+                        + " sum(tapte_dagsverk_gradert_sykemelding) as "
+                        + "sum_tapte_dagsverk_gradert_sykemelding, "
+                        + " sum(tapte_dagsverk) as sum_tapte_dagsverk, "
+                        + " sum(antall_personer) as sum_antall_personer "
+                        + " from sykefravar_statistikk_virksomhet_med_gradering "
+                        + " where "
+                        + " naring_kode in (:naringKoder) "
+                        + " and rectype = :rectype "
+                        + " group by arstall, kvartal"
+                        + " order by arstall, kvartal",
+                MapSqlParameterSource()
+                    .addValue("naringKoder", bransje.identifikatorer)
+                    .addValue("rectype", DatavarehusRepository.RECTYPE_FOR_VIRKSOMHET)
+            ) { rs: ResultSet, _: Int -> mapTilUmaskertSykefraværForEttKvartal(rs) }
+        } catch (e: EmptyResultDataAccessException) {
+            emptyList()
+        }
     }
-  }
 
-  private UmaskertSykefraværForEttKvartal mapTilUmaskertSykefraværForEttKvartal(ResultSet rs)
-      throws SQLException {
-    return new UmaskertSykefraværForEttKvartal(
-        new ÅrstallOgKvartal(rs.getInt("arstall"), rs.getInt("kvartal")),
-        rs.getBigDecimal("sum_tapte_dagsverk_gradert_sykemelding"),
-        rs.getBigDecimal("sum_tapte_dagsverk"),
-        rs.getInt("sum_antall_personer"));
-  }
+    @Throws(SQLException::class)
+    private fun mapTilUmaskertSykefraværForEttKvartal(rs: ResultSet): UmaskertSykefraværForEttKvartal {
+        return UmaskertSykefraværForEttKvartal(
+            ÅrstallOgKvartal(rs.getInt("arstall"), rs.getInt("kvartal")),
+            rs.getBigDecimal("sum_tapte_dagsverk_gradert_sykemelding"),
+            rs.getBigDecimal("sum_tapte_dagsverk"),
+            rs.getInt("sum_antall_personer")
+        )
+    }
 }
