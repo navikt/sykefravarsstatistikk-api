9364d7b24c3250d8e5efe29eae49fd41ed34ac9b
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 15:49:01 +0200

    Java -> Kotlin

--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/sporbarhetslog/Sporbarhetslogg.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/sporbarhetslog/Sporbarhetslogg.kt
@@ -1,91 +1,81 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.sporbarhetslog;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.sporbarhetslog
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.CorrelationIdFilter.CORRELATION_ID_MDC_NAME;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.CorrelationIdFilter
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.stereotype.Component
 
 @Component
-public class Sporbarhetslogg {
-
-  private static final Logger SPORBARHETSLOGGER = LoggerFactory.getLogger("sporbarhetslogger");
-
-  public void loggHendelse(Loggevent event, String kommentar) {
-    List<String> extensions = getExtensions(event);
-    extensions.add("cs5=" + kommentar);
-    extensions.add("cs5Label=Kommentar");
-
-    String loggmelding = lagLoggmelding(extensions, event.getHarTilgang());
-
-    if (event.getHarTilgang()) {
-      SPORBARHETSLOGGER.info(loggmelding);
-    } else {
-      SPORBARHETSLOGGER.warn(loggmelding);
-    }
-  }
-
-  public void loggHendelse(Loggevent event) {
-    String loggmelding = lagLoggmelding(getExtensions(event), event.getHarTilgang());
-
-    if (event.getHarTilgang()) {
-      SPORBARHETSLOGGER.info(loggmelding);
-    } else {
-      SPORBARHETSLOGGER.warn(loggmelding);
-    }
-  }
-
-  private String lagLoggmelding(List<String> extensions, boolean harTilgang) {
-    String version = "CEF:0";
-    String deviceVendor = "sykefravarsstatistikk-api";
-    String deviceProduct = "sporbarhetslogg";
-    String deviceVersion = "1.0";
-    String signatureID = "sykefravarsstatistikk-api:accessed";
-    String name = "sykefravarsstatistikk";
-    String severity = harTilgang ? "INFO" : "WARN";
-
-    String extension = String.join(" ", extensions);
-
-    return String.join(
-        "|",
-        Arrays.asList(
-            version,
-            deviceVendor,
-            deviceProduct,
-            deviceVersion,
-            signatureID,
-            name,
-            severity,
-            extension));
-  }
-
-  private List<String> getExtensions(Loggevent event) {
-    List<String> extensions = new ArrayList<>();
-    extensions.add("end=" + System.currentTimeMillis());
-    extensions.add("suid=" + event.getInnloggetBruker().getFnr().getVerdi());
-    extensions.add("request=" + event.getRequestUrl());
-    extensions.add("requestMethod=" + event.getRequestMethod());
-    extensions.add("cs3=" + (event.getOrgnr() == null ? "" : event.getOrgnr().getVerdi()));
-    extensions.add("cs3Label=OrgNr");
-    String decision = event.getHarTilgang() ? "Permit" : "Deny";
-    extensions.add("flexString1=" + decision);
-    extensions.add("flexString1Label=Decision");
-
-    if (!event.getHarTilgang()) {
-      extensions.add("flexString2=Bruker har ikke rettighet i Altinn");
-      extensions.add("flexString2Label=Begrunnelse");
-      extensions.add("cn1=" + event.getAltinnServiceCode());
-      extensions.add("cn1Label=Service Code");
-      extensions.add("cn2=" + event.getAltinnServiceEdition());
-      extensions.add("cn2Label=Service Edition");
+class Sporbarhetslogg {
+    fun loggHendelse(event: Loggevent, kommentar: String?) {
+        val extensions = getExtensions(event)
+        extensions.add("cs5=$kommentar")
+        extensions.add("cs5Label=Kommentar")
+        val loggmelding = lagLoggmelding(extensions, event.harTilgang)
+        if (event.harTilgang) {
+            SPORBARHETSLOGGER.info(loggmelding)
+        } else {
+            SPORBARHETSLOGGER.warn(loggmelding)
+        }
     }
 
-    extensions.add("sproc=" + MDC.get(CORRELATION_ID_MDC_NAME));
-    return extensions;
-  }
+    fun loggHendelse(event: Loggevent) {
+        val loggmelding = lagLoggmelding(getExtensions(event), event.harTilgang)
+        if (event.harTilgang) {
+            SPORBARHETSLOGGER.info(loggmelding)
+        } else {
+            SPORBARHETSLOGGER.warn(loggmelding)
+        }
+    }
+
+    private fun lagLoggmelding(extensions: List<String>, harTilgang: Boolean): String {
+        val version = "CEF:0"
+        val deviceVendor = "sykefravarsstatistikk-api"
+        val deviceProduct = "sporbarhetslogg"
+        val deviceVersion = "1.0"
+        val signatureID = "sykefravarsstatistikk-api:accessed"
+        val name = "sykefravarsstatistikk"
+        val severity = if (harTilgang) "INFO" else "WARN"
+        val extension = java.lang.String.join(" ", extensions)
+        return java.lang.String.join(
+            "|",
+            listOf(
+                version,
+                deviceVendor,
+                deviceProduct,
+                deviceVersion,
+                signatureID,
+                name,
+                severity,
+                extension
+            )
+        )
+    }
+
+    private fun getExtensions(event: Loggevent): MutableList<String> {
+        val extensions: MutableList<String> = ArrayList()
+        extensions.add("end=" + System.currentTimeMillis())
+        extensions.add("suid=" + event.innloggetBruker!!.fnr.verdi)
+        extensions.add("request=" + event.requestUrl)
+        extensions.add("requestMethod=" + event.requestMethod)
+        extensions.add("cs3=" + if (event.orgnr == null) "" else event.orgnr.verdi)
+        extensions.add("cs3Label=OrgNr")
+        val decision = if (event.harTilgang) "Permit" else "Deny"
+        extensions.add("flexString1=$decision")
+        extensions.add("flexString1Label=Decision")
+        if (!event.harTilgang) {
+            extensions.add("flexString2=Bruker har ikke rettighet i Altinn")
+            extensions.add("flexString2Label=Begrunnelse")
+            extensions.add("cn1=" + event.altinnServiceCode)
+            extensions.add("cn1Label=Service Code")
+            extensions.add("cn2=" + event.altinnServiceEdition)
+            extensions.add("cn2Label=Service Edition")
+        }
+        extensions.add("sproc=" + MDC.get(CorrelationIdFilter.CORRELATION_ID_MDC_NAME))
+        return extensions
+    }
+
+    companion object {
+        private val SPORBARHETSLOGGER = LoggerFactory.getLogger("sporbarhetslogger")
+    }
 }
--- src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/tokenx/TokenXException.kt
+++ src/main/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/infrastruktur/tokenx/TokenXException.kt
@@ -1,7 +1,3 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.tokenx;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.tokenx
 
-public class TokenXException extends Exception {
-  public TokenXException(String msg) {
-    super(msg);
-  }
-}
+class TokenXException(msg: String?) : Exception(msg)
