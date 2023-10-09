d9b16019886b5effe0715045979e6e3268bfe307
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 10:27:25 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/KafkaUtsendingHistorikkData.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/KafkaUtsendingHistorikkData.kt
@@ -1,19 +1,7 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api
 
-import java.time.LocalDateTime;
+import java.time.LocalDateTime
 
-public class KafkaUtsendingHistorikkData {
-
-  public String orgnr;
-  public String key;
-  public String value;
-  public LocalDateTime opprettet;
-
-  public KafkaUtsendingHistorikkData(
-      String orgnr, String key, String value, LocalDateTime opprettet) {
-    this.orgnr = orgnr;
-    this.key = key;
-    this.value = value;
-    this.opprettet = opprettet;
-  }
-}
+class KafkaUtsendingHistorikkData(
+  @JvmField var orgnr: String, @JvmField var key: String, @JvmField var value: String, @JvmField var opprettet: LocalDateTime
+)
