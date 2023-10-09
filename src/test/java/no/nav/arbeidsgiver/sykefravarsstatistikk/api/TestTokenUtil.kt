18da42d3bfa4b007cec2edf2d8bd34d1dfc07e72
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 10:24:08 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/TestTokenUtil.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/TestTokenUtil.kt
@@ -1,40 +1,36 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api
 
-import no.nav.security.mock.oauth2.MockOAuth2Server;
-import org.apache.commons.lang3.StringUtils;
+import no.nav.security.mock.oauth2.MockOAuth2Server
 
-import java.util.HashMap;
-import java.util.Map;
+object TestTokenUtil {
+    @JvmField
+    var TOKENX_ISSUER_ID = "tokenx"
 
-public class TestTokenUtil {
-
-  public static String TOKENX_ISSUER_ID = "tokenx";
-
-  public static String createMockIdportenTokenXToken(MockOAuth2Server mockOAuth2Server) {
-    return createToken(
-        mockOAuth2Server,
-        "15008462396",
-        TOKENX_ISSUER_ID,
-        "https://oidc.difi.no/idporten-oidc-provider/");
-  }
-
-  public static String createToken(
-      MockOAuth2Server oAuth2Server, String pid, String issuerId, String idp) {
-
-    return createToken(oAuth2Server, pid, "IKKE_I_BRUK_LENGER", issuerId, idp);
-  }
-
-  public static String createToken(
-      MockOAuth2Server oAuth2Server, String pid, String sub, String issuerId, String idp) {
-    Map<String, String> claims = new HashMap<>();
-
-    if (StringUtils.isNoneEmpty(idp)) {
-      claims.put("idp", idp);
+    @JvmStatic
+    fun createMockIdportenTokenXToken(mockOAuth2Server: MockOAuth2Server): String {
+        return createToken(
+            mockOAuth2Server,
+            "15008462396",
+            TOKENX_ISSUER_ID,
+            "https://oidc.difi.no/idporten-oidc-provider/"
+        )
     }
 
-    if (StringUtils.isNoneEmpty(pid)) {
-      claims.put("pid", pid);
+    @JvmStatic
+    fun createToken(
+        oAuth2Server: MockOAuth2Server, pid: String?, issuerId: String?, idp: String?
+    ): String {
+        return createToken(oAuth2Server, pid, "IKKE_I_BRUK_LENGER", issuerId, idp)
     }
-      return oAuth2Server.issueToken(issuerId, sub, "someaudience", claims).serialize();
-  }
+
+    @JvmStatic
+    fun createToken(
+        oAuth2Server: MockOAuth2Server, pid: String?, sub: String?, issuerId: String?, idp: String?
+    ): String {
+        val claims = listOf(
+            "idp" to idp,
+            "pid" to pid,
+        ).filter { it.second != null }.toMap() as Map<String, String>
+        return oAuth2Server.issueToken(issuerId!!, sub!!, "someaudience", claims).serialize()
+    }
 }
