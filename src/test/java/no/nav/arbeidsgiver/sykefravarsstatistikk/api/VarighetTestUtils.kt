2bb227832db0357cca71c8bbcdb84e924d937df3
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 10:06:59 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/VarighetTestUtils.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/VarighetTestUtils.kt
@@ -1,98 +1,108 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api
 
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Næringskode;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ÅrstallOgKvartal;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Varighetskategori;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Næringskode
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Varighetskategori
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ÅrstallOgKvartal
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
-public class VarighetTestUtils {
+object VarighetTestUtils {
+    @JvmStatic
+    fun leggTilVirksomhetsstatistikkMedVarighet(
+        jdbcTemplate: NamedParameterJdbcTemplate,
+        orgnr: String?,
+        årstallOgKvartal: ÅrstallOgKvartal,
+        varighetskategori: Varighetskategori,
+        antallPersoner: Int,
+        tapteDagsverk: Int,
+        muligeDagsverk: Int
+    ) {
+        jdbcTemplate.update(
+            "insert into sykefravar_statistikk_virksomhet (arstall, kvartal, orgnr, varighet, "
+                    + "antall_personer, tapte_dagsverk, mulige_dagsverk) "
+                    + "VALUES (:arstall, :kvartal, :orgnr, :varighet, :antall_personer, "
+                    + ":tapte_dagsverk, :mulige_dagsverk)",
+            MapSqlParameterSource()
+                .addValue("arstall", årstallOgKvartal.årstall)
+                .addValue("kvartal", årstallOgKvartal.kvartal)
+                .addValue("orgnr", orgnr)
+                .addValue("varighet", varighetskategori.kode)
+                .addValue("antall_personer", antallPersoner)
+                .addValue("tapte_dagsverk", tapteDagsverk)
+                .addValue("mulige_dagsverk", muligeDagsverk)
+        )
+    }
 
-  public static void leggTilVirksomhetsstatistikkMedVarighet(
-      NamedParameterJdbcTemplate jdbcTemplate,
-      String orgnr,
-      ÅrstallOgKvartal årstallOgKvartal,
-      Varighetskategori varighetskategori,
-      int antallPersoner,
-      int tapteDagsverk,
-      int muligeDagsverk) {
-    jdbcTemplate.update(
-        "insert into sykefravar_statistikk_virksomhet (arstall, kvartal, orgnr, varighet, "
-            + "antall_personer, tapte_dagsverk, mulige_dagsverk) "
-            + "VALUES (:arstall, :kvartal, :orgnr, :varighet, :antall_personer, "
-            + ":tapte_dagsverk, :mulige_dagsverk)",
-        new MapSqlParameterSource()
-            .addValue("arstall", årstallOgKvartal.getÅrstall())
-            .addValue("kvartal", årstallOgKvartal.getKvartal())
-            .addValue("orgnr", orgnr)
-            .addValue("varighet", varighetskategori.kode)
-            .addValue("antall_personer", antallPersoner)
-            .addValue("tapte_dagsverk", tapteDagsverk)
-            .addValue("mulige_dagsverk", muligeDagsverk));
-  }
+    @JvmStatic
+    fun leggTilStatisitkkNæringMedVarighetForTotalVarighetskategori(
+        jdbcTemplate: NamedParameterJdbcTemplate,
+        næringskode: Næringskode,
+        årstallOgKvartal: ÅrstallOgKvartal,
+        antallPersoner: Int,
+        muligeDagsverk: Int
+    ) {
+        leggTilStatisitkkNæringMedVarighet(
+            jdbcTemplate,
+            næringskode,
+            årstallOgKvartal.årstall,
+            årstallOgKvartal.kvartal,
+            Varighetskategori.TOTAL.kode,
+            antallPersoner,
+            0,
+            muligeDagsverk
+        )
+    }
 
-  public static void leggTilStatisitkkNæringMedVarighetForTotalVarighetskategori(
-          NamedParameterJdbcTemplate jdbcTemplate,
-          Næringskode næringskode,
-          ÅrstallOgKvartal årstallOgKvartal,
-          int antallPersoner,
-          int muligeDagsverk) {
-    leggTilStatisitkkNæringMedVarighet(
-        jdbcTemplate,
-        næringskode,
-        årstallOgKvartal.getÅrstall(),
-        årstallOgKvartal.getKvartal(),
-        Varighetskategori.TOTAL.kode,
-        antallPersoner,
-        0,
-        muligeDagsverk);
-  }
+    @JvmStatic
+    fun leggTilStatisitkkNæringMedVarighet(
+        jdbcTemplate: NamedParameterJdbcTemplate,
+        næringskode: Næringskode,
+        årstallOgKvartal: ÅrstallOgKvartal,
+        varighetskategori: Varighetskategori,
+        tapteDagsverk: Int
+    ) {
+        leggTilStatisitkkNæringMedVarighet(
+            jdbcTemplate,
+            næringskode,
+            årstallOgKvartal.årstall,
+            årstallOgKvartal.kvartal,
+            varighetskategori.kode,
+            0,
+            tapteDagsverk,
+            0
+        )
+    }
 
-  public static void leggTilStatisitkkNæringMedVarighet(
-          NamedParameterJdbcTemplate jdbcTemplate,
-          Næringskode næringskode,
-          ÅrstallOgKvartal årstallOgKvartal,
-          Varighetskategori varighetskategori,
-          int tapteDagsverk) {
-    leggTilStatisitkkNæringMedVarighet(
-        jdbcTemplate,
-        næringskode,
-        årstallOgKvartal.getÅrstall(),
-        årstallOgKvartal.getKvartal(),
-        varighetskategori.kode,
-        0,
-        tapteDagsverk,
-        0);
-  }
-
-  public static void leggTilStatisitkkNæringMedVarighet(
-          NamedParameterJdbcTemplate jdbcTemplate,
-          Næringskode næringskode,
-          int årstall,
-          int kvartal,
-          String varighet,
-          int antallPersoner,
-          int tapteDagsverk,
-          int muligeDagsverk) {
-    jdbcTemplate.update(
-        "insert into sykefravar_statistikk_naring_med_varighet "
-            + "(arstall, kvartal, naring_kode, varighet, antall_personer, tapte_dagsverk, "
-            + "mulige_dagsverk) "
-            + "VALUES ("
-            + ":arstall, "
-            + ":kvartal, "
-            + ":naring_kode, "
-            + ":varighet, "
-            + ":antall_personer, "
-            + ":tapte_dagsverk, "
-            + ":mulige_dagsverk)",
-        new MapSqlParameterSource()
-            .addValue("arstall", årstall)
-            .addValue("kvartal", kvartal)
-            .addValue("naring_kode", næringskode.getFemsifferIdentifikator())
-            .addValue("varighet", varighet)
-            .addValue("antall_personer", antallPersoner)
-            .addValue("tapte_dagsverk", tapteDagsverk)
-            .addValue("mulige_dagsverk", muligeDagsverk));
-  }
+    fun leggTilStatisitkkNæringMedVarighet(
+        jdbcTemplate: NamedParameterJdbcTemplate,
+        næringskode: Næringskode,
+        årstall: Int,
+        kvartal: Int,
+        varighet: String?,
+        antallPersoner: Int,
+        tapteDagsverk: Int,
+        muligeDagsverk: Int
+    ) {
+        jdbcTemplate.update(
+            "insert into sykefravar_statistikk_naring_med_varighet "
+                    + "(arstall, kvartal, naring_kode, varighet, antall_personer, tapte_dagsverk, "
+                    + "mulige_dagsverk) "
+                    + "VALUES ("
+                    + ":arstall, "
+                    + ":kvartal, "
+                    + ":naring_kode, "
+                    + ":varighet, "
+                    + ":antall_personer, "
+                    + ":tapte_dagsverk, "
+                    + ":mulige_dagsverk)",
+            MapSqlParameterSource()
+                .addValue("arstall", årstall)
+                .addValue("kvartal", kvartal)
+                .addValue("naring_kode", næringskode.femsifferIdentifikator)
+                .addValue("varighet", varighet)
+                .addValue("antall_personer", antallPersoner)
+                .addValue("tapte_dagsverk", tapteDagsverk)
+                .addValue("mulige_dagsverk", muligeDagsverk)
+        )
+    }
 }
