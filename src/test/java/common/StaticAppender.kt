ec46a246856002d5e55e6875141832e9fd4fe9f6
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 15:09:02 +0200

    Java -> Kotlin

--- src/test/java/common/StaticAppender.kt
+++ src/test/java/common/StaticAppender.kt
@@ -1,28 +1,18 @@
-package common;
+package common
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.AppenderBase;
-import java.util.ArrayList;
-import java.util.List;
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
 
-public class StaticAppender extends AppenderBase<ILoggingEvent> {
+class StaticAppender : AppenderBase<ILoggingEvent>() {
+    public override fun append(e: ILoggingEvent) {
+        events.add(e)
+    }
 
-  static List<ILoggingEvent> events = new ArrayList<>();
+    companion object {
+        var events: MutableList<ILoggingEvent> = ArrayList()
 
-  @Override
-  public void append(ILoggingEvent e) {
-    events.add(e);
-  }
-
-  public static List<ILoggingEvent> getEvents() {
-    return events;
-  }
-
-  public static void clearEvents() {
-    events.clear();
-  }
-
-  public static ILoggingEvent getLastLoggedEvent() {
-    return getEvents().get(0);
-  }
+        fun clearEvents() {
+            events.clear()
+        }
+    }
 }
