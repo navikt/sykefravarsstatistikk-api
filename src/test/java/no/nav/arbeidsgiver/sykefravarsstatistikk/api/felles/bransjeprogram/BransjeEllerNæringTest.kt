bd845992534f2c723fc94298692d1433f766028e
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 14:26:36 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/felles/bransjeprogram/BransjeEllerNæringTest.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/felles/bransjeprogram/BransjeEllerNæringTest.kt
@@ -1,35 +1,29 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.bransjeprogram;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.felles.bransjeprogram
 
-import ia.felles.definisjoner.bransjer.Bransjer;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Næring;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.Bransje;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.BransjeEllerNæring;
-import org.junit.jupiter.api.Test;
+import ia.felles.definisjoner.bransjer.Bransjer
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.Næring
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.Bransje
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.bransjeprogram.BransjeEllerNæring
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
 
-import java.util.NoSuchElementException;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-class BransjeEllerNæringTest {
-
+internal class BransjeEllerNæringTest {
     @Test
-    public void BransjeEllerNæring_kan_opprettes_for_en_bransje_og_returnerer_en_bransje() {
-        Bransje bransje = new Bransje(Bransjer.BARNEHAGER);
-        BransjeEllerNæring bransjeEllerNæring = new BransjeEllerNæring(bransje);
-
-        assertThat(bransjeEllerNæring.isBransje()).isEqualTo(true);
-        assertThat(bransjeEllerNæring.getBransje()).isEqualTo(bransje);
-        assertThrows(NoSuchElementException.class, bransjeEllerNæring::getNæring);
+    fun BransjeEllerNæring_kan_opprettes_for_en_bransje_og_returnerer_en_bransje() {
+        val bransje = Bransje(Bransjer.BARNEHAGER)
+        val bransjeEllerNæring = BransjeEllerNæring(bransje)
+        assertThat(bransjeEllerNæring.isBransje).isEqualTo(true)
+        assertThat(bransjeEllerNæring.getBransje()).isEqualTo(bransje)
+        assertThrows(NoSuchElementException::class.java) { bransjeEllerNæring.næring }
     }
 
     @Test
-    public void BransjeEllerNæring_kan_opprettes_for_en_næring_og_returnerer_en_næring() {
-        Næring næring = new Næring("61");
-        BransjeEllerNæring bransjeEllerNæring = new BransjeEllerNæring(næring);
-
-        assertThat(bransjeEllerNæring.isBransje()).isEqualTo(false);
-        assertThat(bransjeEllerNæring.getNæring()).isEqualTo(næring);
-        assertThrows(NoSuchElementException.class, bransjeEllerNæring::getBransje);
+    fun BransjeEllerNæring_kan_opprettes_for_en_næring_og_returnerer_en_næring() {
+        val næring = Næring("61")
+        val bransjeEllerNæring = BransjeEllerNæring(næring)
+        assertThat(bransjeEllerNæring.isBransje).isEqualTo(false)
+        assertThat(bransjeEllerNæring.næring).isEqualTo(næring)
+        assertThrows(NoSuchElementException::class.java) { bransjeEllerNæring.getBransje() }
     }
 }
