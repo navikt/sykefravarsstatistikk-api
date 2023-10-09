ea57e0f3efc112b5e7615c082340b2d47f456336
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 14:14:34 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/publiseringsdatoer/PubliseringsdatoerRepositoryTest.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/publiseringsdatoer/PubliseringsdatoerRepositoryTest.kt
@@ -1,56 +1,46 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.publiseringsdatoer;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.publiseringsdatoer
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import common.StaticAppender;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.ImporttidspunktDto;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database.PubliseringsdatoerRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import common.StaticAppender
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database.PubliseringsdatoerRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-class PubliseringsdatoerRepositoryTest {
-    PubliseringsdatoerRepository publiseringsdatoerRepository;
+@ExtendWith(MockitoExtension::class)
+internal class PubliseringsdatoerRepositoryTest {
+    var publiseringsdatoerRepository: PubliseringsdatoerRepository? = null
 
     @Mock
-    NamedParameterJdbcTemplate mockJdbcTemplate;
-
+    var mockJdbcTemplate: NamedParameterJdbcTemplate? = null
     @BeforeEach
-    void setUp() {
-        publiseringsdatoerRepository = new PubliseringsdatoerRepository(mockJdbcTemplate);
-        StaticAppender.clearEvents();
+    fun setUp() {
+        publiseringsdatoerRepository = PubliseringsdatoerRepository(mockJdbcTemplate!!)
+        StaticAppender.clearEvents()
     }
 
     @AfterEach
-    void tearDown() {
-        reset(mockJdbcTemplate);
+    fun tearDown() {
+        Mockito.reset(mockJdbcTemplate)
     }
 
     @Test
-    void
-    hentSistePubliseringstidspunkt_nårPubliseringsdatoIkkeBlirFunnet_skalReturnereTomOptionalOgLoggeError() {
-        when(mockJdbcTemplate.query(anyString(), anyRowMapper())).thenReturn(List.of());
-
-        ImporttidspunktDto faktisk = publiseringsdatoerRepository.hentSisteImporttidspunkt();
-        assertNull(faktisk);
+    fun hentSistePubliseringstidspunkt_nårPubliseringsdatoIkkeBlirFunnet_skalReturnereTomOptionalOgLoggeError() {
+        whenever(mockJdbcTemplate!!.query(ArgumentMatchers.anyString(), anyRowMapper())).thenReturn(listOf())
+        val faktisk = publiseringsdatoerRepository!!.hentSisteImporttidspunkt()
+        Assertions.assertNull(faktisk)
     }
 
-    private RowMapper<?> anyRowMapper() {
-        return Mockito.any();
+    private fun anyRowMapper(): RowMapper<*> {
+        return any()
     }
 }
