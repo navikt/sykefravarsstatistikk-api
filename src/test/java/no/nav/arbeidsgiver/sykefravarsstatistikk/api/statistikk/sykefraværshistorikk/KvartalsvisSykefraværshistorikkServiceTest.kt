a9f92fcd8070c43a9239395c3ad5289b37fd35b5
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 14:12:15 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/KvartalsvisSykefraværshistorikkServiceTest.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/statistikk/sykefraværshistorikk/KvartalsvisSykefraværshistorikkServiceTest.kt
@@ -1,77 +1,77 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api.statistikk.sykefraværshistorikk
 
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.aggregering.KvartalsvisSykefraværshistorikkService;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.*;
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database.KvartalsvisSykefraværRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestData.enNæringskode
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestData.etOrgnr
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.aggregering.KvartalsvisSykefraværshistorikkService
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.domenemodeller.*
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database.KvartalsvisSykefraværRepository
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import java.math.BigDecimal
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestData.enNæringskode;
-import static no.nav.arbeidsgiver.sykefravarsstatistikk.api.TestData.etOrgnr;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-public class KvartalsvisSykefraværshistorikkServiceTest {
-
+@ExtendWith(MockitoExtension::class)
+class KvartalsvisSykefraværshistorikkServiceTest {
     @Mock
-    private KvartalsvisSykefraværRepository kvartalsvisSykefraværprosentRepository;
-
-    KvartalsvisSykefraværshistorikkService kvartalsvisSykefraværshistorikkService;
+    private val kvartalsvisSykefraværprosentRepository: KvartalsvisSykefraværRepository? = null
+    var kvartalsvisSykefraværshistorikkService: KvartalsvisSykefraværshistorikkService? = null
 
     @BeforeEach
-    public void setUp() {
-        kvartalsvisSykefraværshistorikkService =
-                new KvartalsvisSykefraværshistorikkService(
-                        kvartalsvisSykefraværprosentRepository
-                );
-
-        when(kvartalsvisSykefraværprosentRepository.hentKvartalsvisSykefraværprosentLand())
-                .thenReturn(List.of(sykefraværprosent()));
-        when(kvartalsvisSykefraværprosentRepository.hentKvartalsvisSykefraværprosentSektor(any()))
-                .thenReturn(List.of(sykefraværprosent()));
-        when(kvartalsvisSykefraværprosentRepository.hentKvartalsvisSykefraværprosentVirksomhet(any()))
-                .thenReturn(List.of(sykefraværprosent()));
+    fun setUp() {
+        kvartalsvisSykefraværshistorikkService = KvartalsvisSykefraværshistorikkService(
+            kvartalsvisSykefraværprosentRepository!!
+        )
+        whenever(kvartalsvisSykefraværprosentRepository.hentKvartalsvisSykefraværprosentLand())
+            .thenReturn(listOf(sykefraværprosent()))
+        whenever(kvartalsvisSykefraværprosentRepository.hentKvartalsvisSykefraværprosentSektor(ArgumentMatchers.any()))
+            .thenReturn(listOf(sykefraværprosent()))
+        whenever(
+            kvartalsvisSykefraværprosentRepository.hentKvartalsvisSykefraværprosentVirksomhet(
+                ArgumentMatchers.any()
+            )
+        )
+            .thenReturn(listOf(sykefraværprosent()))
     }
 
     @Test
-    public void
-    hentSykefraværshistorikk__skal_returnere_en_næring_dersom_virksomhet_er_i_bransjeprogram_på_2_siffer_nivå() {
-        UnderenhetLegacy underenhet =
-                new UnderenhetLegacy(etOrgnr(), etOrgnr(), "Underenhet AS", enNæringskode("10300"), 40);
-
-        List<KvartalsvisSykefraværshistorikk> kvartalsvisSykefraværshistorikk =
-                kvartalsvisSykefraværshistorikkService.hentSykefraværshistorikk(
-                        underenhet, Sektor.PRIVAT);
-
-        assertThatHistorikkHarKategori(kvartalsvisSykefraværshistorikk, Statistikkategori.NÆRING, true);
-        assertThatHistorikkHarKategori(kvartalsvisSykefraværshistorikk, Statistikkategori.LAND, true);
-        assertThatHistorikkHarKategori(kvartalsvisSykefraværshistorikk, Statistikkategori.SEKTOR, true);
+    fun hentSykefraværshistorikk__skal_returnere_en_næring_dersom_virksomhet_er_i_bransjeprogram_på_2_siffer_nivå() {
+        val underenhet = UnderenhetLegacy(etOrgnr(), etOrgnr(), "Underenhet AS", enNæringskode("10300"), 40)
+        val kvartalsvisSykefraværshistorikk: List<KvartalsvisSykefraværshistorikk> =
+            kvartalsvisSykefraværshistorikkService!!.hentSykefraværshistorikk(
+                underenhet, Sektor.PRIVAT
+            )
+        assertThatHistorikkHarKategori(kvartalsvisSykefraværshistorikk, Statistikkategori.NÆRING, true)
+        assertThatHistorikkHarKategori(kvartalsvisSykefraværshistorikk, Statistikkategori.LAND, true)
+        assertThatHistorikkHarKategori(kvartalsvisSykefraværshistorikk, Statistikkategori.SEKTOR, true)
         assertThatHistorikkHarKategori(
-                kvartalsvisSykefraværshistorikk, Statistikkategori.VIRKSOMHET, true);
+            kvartalsvisSykefraværshistorikk, Statistikkategori.VIRKSOMHET, true
+        )
         assertThatHistorikkHarKategori(
-                kvartalsvisSykefraværshistorikk, Statistikkategori.BRANSJE, false);
+            kvartalsvisSykefraværshistorikk, Statistikkategori.BRANSJE, false
+        )
     }
 
-    private static void assertThatHistorikkHarKategori(
-            List<KvartalsvisSykefraværshistorikk> kvartalsvisSykefraværshistorikk,
-            Statistikkategori statistikkategori,
-            boolean expected) {
-        assertThat(
+    companion object {
+        private fun assertThatHistorikkHarKategori(
+            kvartalsvisSykefraværshistorikk: List<KvartalsvisSykefraværshistorikk>,
+            statistikkategori: Statistikkategori,
+            expected: Boolean
+        ) {
+            Assertions.assertThat(
                 kvartalsvisSykefraværshistorikk.stream()
-                        .anyMatch(historikk -> historikk.getType().equals(statistikkategori)))
-                .isEqualTo(expected);
-    }
+                    .anyMatch { (type): KvartalsvisSykefraværshistorikk -> type == statistikkategori })
+                .isEqualTo(expected)
+        }
 
-    private static SykefraværForEttKvartal sykefraværprosent() {
-        return new SykefraværForEttKvartal(
-                new ÅrstallOgKvartal(2019, 1), new BigDecimal(50), new BigDecimal(100), 10);
+        private fun sykefraværprosent(): SykefraværForEttKvartal {
+            return SykefraværForEttKvartal(
+                ÅrstallOgKvartal(2019, 1), BigDecimal(50), BigDecimal(100), 10
+            )
+        }
     }
 }
