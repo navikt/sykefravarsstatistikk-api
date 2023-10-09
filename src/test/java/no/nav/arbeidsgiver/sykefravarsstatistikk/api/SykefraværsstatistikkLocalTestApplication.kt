e4e5717e1cddf99c785150a1c08a730b77879d60
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-09 10:25:56 +0200

    Java -> Kotlin

--- src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/SykefraværsstatistikkLocalTestApplication.kt
+++ src/test/java/no/nav/arbeidsgiver/sykefravarsstatistikk/api/SykefraværsstatistikkLocalTestApplication.kt
@@ -1,23 +1,21 @@
-package no.nav.arbeidsgiver.sykefravarsstatistikk.api;
+package no.nav.arbeidsgiver.sykefravarsstatistikk.api
 
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Profile;
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
+import org.springframework.context.annotation.Profile
 
-@Profile({"local", "mvc-test"})
+@Profile("local", "mvc-test")
 @SpringBootApplication
 @ComponentScan(
-    basePackages = {"no.nav.arbeidsgiver"},
-    excludeFilters = {
-      @ComponentScan.Filter(
-          type = FilterType.ASSIGNABLE_TYPE,
-          value = {
-            AppConfigForJdbcTesterConfig.class,
-          })
-    })
-@EnableJwtTokenValidation(ignore = {"org.springframework", "org.springdoc"})
+    basePackages = ["no.nav.arbeidsgiver"],
+    excludeFilters = [ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        value = [AppConfigForJdbcTesterConfig::class]
+    )]
+)
+@EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 @EnableConfigurationProperties
-public class SykefraværsstatistikkLocalTestApplication {}
+open class SykefraværsstatistikkLocalTestApplication
