6d7df0f83d4e4c48a652796a49fcb0113e5f6d8d
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-20 12:21:20 +0200

    Fjern exception

--- src/main/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/publiseringsdatoApi/PubliseringsdatoerService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/publiseringsdatoApi/PubliseringsdatoerService.kt
@@ -3,9 +3,7 @@ package no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.publiseringsda
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.fellesdomene.ÅrstallOgKvartal
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database.ImporttidspunktRepository
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.infrastruktur.database.PubliseringsdatoerRepository
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.ResponseStatus
 
 @Service
 class PubliseringsdatoerService(
@@ -13,9 +11,8 @@ class PubliseringsdatoerService(
     private val importtidspunktRepository: ImporttidspunktRepository
 ) {
 
-    fun hentSistePubliserteKvartal(): ÅrstallOgKvartal {
+    fun hentSistePubliserteKvartal(): ÅrstallOgKvartal? {
         return importtidspunktRepository.hentNyesteImporterteKvartal()?.gjeldendePeriode
-            ?: throw PubliseringsdatoerDatauthentingFeil("Kunne ikke hente ut siste publiserte kvartal")
     }
 
     fun hentPubliseringsdatoer(): Publiseringsdatoer? {
@@ -34,9 +31,4 @@ class PubliseringsdatoerService(
             nestePubliseringsdato = nestePubliseringsdato
         )
     }
-
 }
-
-
-@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
-class PubliseringsdatoerDatauthentingFeil(message: String?) : RuntimeException(message)
