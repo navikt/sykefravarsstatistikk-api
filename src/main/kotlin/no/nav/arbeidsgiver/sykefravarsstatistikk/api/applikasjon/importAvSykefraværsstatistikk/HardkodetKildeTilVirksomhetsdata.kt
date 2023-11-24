26fb2a2a9267f5967914ace3e5b55c3de12cf837
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-11-24 13:19:37 +0100

    Legg til nye testdata for virksomheter

--- src/main/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/importAvSykefraværsstatistikk/HardkodetKildeTilVirksomhetsdata.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/importAvSykefraværsstatistikk/HardkodetKildeTilVirksomhetsdata.kt
@@ -1,10 +1,12 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.importAvSykefraværsstatistikk
 
+import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import com.fasterxml.jackson.dataformat.csv.CsvSchema
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.eksportAvSykefraværsstatistikk.KildeTilVirksomhetsdata
-import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.importAvSykefraværsstatistikk.domene.Orgenhet
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.fellesdomene.Orgnr
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.fellesdomene.Sektor
 import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.fellesdomene.ÅrstallOgKvartal
+import no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.importAvSykefraværsstatistikk.domene.Orgenhet
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
@@ -12,18 +14,38 @@ import org.springframework.stereotype.Component
 @Component
 @Profile("dev")
 @Primary
-class HardkodetKildeTilVirksomhetsdata : KildeTilVirksomhetsdata {
+object HardkodetKildeTilVirksomhetsdata : KildeTilVirksomhetsdata {
     override fun hentVirksomheter(årstallOgKvartal: ÅrstallOgKvartal): List<Orgenhet> {
-        return SykefraværsstatistikkImporteringUtils.genererSykefraværsstatistikkVirksomhetMedGradering(årstallOgKvartal).map {
+        val csvSchema = CsvSchema.builder()
+            .addColumn("orgnr")
+            .addColumn("sektor")
+            .addColumn("primærnæring")
+            .setUseHeader(true)
+            .build()
+        val virksomheter = CsvMapper()
+            .readerFor(DevVirksomhet::class.java)
+            .with(csvSchema)
+            .readValues<DevVirksomhet>(this::class.java.getResource("/mock/test-virksomheter-FIA.csv"))
+            .readAll()
+            .toList()
+
+        return virksomheter.map {
+            val næringskode = it.primærnæring.replace(".", "")
             Orgenhet(
-                orgnr = Orgnr(verdi = it.orgnr),
-                navn = "Bedrift ${it.orgnr}",
-                rectype = it.rectype,
-                sektor = Sektor.PRIVAT,
-                næring = it.næring,
-                næringskode = it.næringkode,
+                orgnr = Orgnr(it.orgnr),
+                navn = "Virksomhet ${it.orgnr}",
+                rectype = "2",
+                sektor = Sektor.fraSektorkode(it.sektor),
+                næring = næringskode.take(2),
+                næringskode = næringskode,
                 årstallOgKvartal = årstallOgKvartal
             )
         }
     }
+
+    data class DevVirksomhet(
+        val orgnr: String = "",
+        val sektor: String = "",
+        val primærnæring: String = "",
+    )
 }
