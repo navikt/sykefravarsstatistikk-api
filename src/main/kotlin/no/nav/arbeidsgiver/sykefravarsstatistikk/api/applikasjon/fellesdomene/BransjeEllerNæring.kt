c0da3f3f2c505d86b6c0731d8069a8c39589a3eb
Odd Gunnar <odd.gunnar.fatland@nav.no>
2023-12-20 14:15:46 +0100

    Fjern vavr som avhengighet (#441)

--- src/main/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/fellesdomene/BransjeEllerNæring.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/fellesdomene/BransjeEllerNæring.kt
@@ -1,36 +1,27 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.fellesdomene
 
 import ia.felles.definisjoner.bransjer.Bransje
-import io.vavr.control.Either
 
 class BransjeEllerNæring {
-    val verdi: Either<Bransje, Næring>
+    val bransje: Bransje?
+    val næring: Næring?
 
     constructor(bransje: Bransje) {
-        verdi = Either.left(bransje)
+        this.bransje = bransje
+        this.næring = null
     }
 
     constructor(næring: Næring) {
-        verdi = Either.right(næring)
+        this.bransje = null
+        this.næring = næring
     }
 
     val statistikkategori: Statistikkategori
-        get() = if (verdi.isLeft) {
+        get() = if (bransje != null) {
             Statistikkategori.BRANSJE
         } else {
             Statistikkategori.NÆRING
         }
-    val isBransje: Boolean
-        get() = verdi.isLeft
 
-    fun getBransje(): Bransje {
-        return verdi.left
-    }
-
-    fun navn(): String {
-        return if (isBransje) verdi.left.navn else verdi.get().navn
-    }
-
-    val næring: Næring
-        get() = verdi.get()
+    fun navn(): String = bransje?.navn ?: næring!!.navn
 }
