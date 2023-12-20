e70e64f461e1741bd1ba887d12f46e07ebd8bd9e
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-12-20 14:10:53 +0100

    Fjern vavr som avhengighet

--- src/test/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/fellesdomene/BransjeEllerNæringTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/sykefravarsstatistikk/api/applikasjon/fellesdomene/BransjeEllerNæringTest.kt
@@ -1,25 +1,23 @@
 package no.nav.arbeidsgiver.sykefravarsstatistikk.api.applikasjon.fellesdomene
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.Test
 import ia.felles.definisjoner.bransjer.Bransje
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 internal class BransjeEllerNæringTest {
     @Test
     fun BransjeEllerNæring_kan_opprettes_for_en_bransje_og_returnerer_en_bransje() {
         val bransje = Bransje.BARNEHAGER
         val bransjeEllerNæring = BransjeEllerNæring(bransje)
-        assertThat(bransjeEllerNæring.isBransje).isEqualTo(true)
-        assertThat(bransjeEllerNæring.getBransje()).isEqualTo(bransje)
-        assertThrows(NoSuchElementException::class.java) { bransjeEllerNæring.næring }
+        assertThat(bransjeEllerNæring.bransje).isEqualTo(bransje)
+        assertThat(bransjeEllerNæring.næring).isEqualTo(null)
     }
 
     @Test
     fun BransjeEllerNæring_kan_opprettes_for_en_næring_og_returnerer_en_næring() {
         val næring = Næring("61")
         val bransjeEllerNæring = BransjeEllerNæring(næring)
-        assertThat(bransjeEllerNæring.isBransje).isEqualTo(false)
         assertThat(bransjeEllerNæring.næring).isEqualTo(næring)
-        assertThrows(NoSuchElementException::class.java) { bransjeEllerNæring.getBransje() }
+        assertThat(bransjeEllerNæring.bransje).isEqualTo(null)
     }
 }
