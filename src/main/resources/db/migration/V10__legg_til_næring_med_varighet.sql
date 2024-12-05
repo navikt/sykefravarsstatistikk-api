b2fa1206d0d57196ec3d9b094757ce8ae5f6ed59
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-12-05 09:36:08 +0100

    Endre charset til en Flyway migreringsscript (us-ascii --> UTF-8) Deploy til dev

--- src/main/resources/db/migration/V10__legg_til_næring_med_varighet.sql
+++ src/main/resources/db/migration/V10__legg_til_næring_med_varighet.sql
@@ -1,11 +1,12 @@
-create table sykefravar_statistikk_naring_med_varighet (
-  id serial primary key,
-  naring_kode varchar not null,
-  varighet varchar(1) not null,
-  arstall smallint not null,
-  kvartal smallint not null,
-  antall_personer numeric(17,0) not null,
-  tapte_dagsverk numeric(17,6) not null,
-  mulige_dagsverk numeric(17,6) not null,
-  opprettet timestamp default current_timestamp
+create table sykefravar_statistikk_naring_med_varighet
+(
+    id              serial primary key,
+    naring_kode     varchar        not null,
+    varighet        varchar(1)     not null,
+    arstall         smallint       not null,
+    kvartal         smallint       not null,
+    antall_personer numeric(17, 0) not null,
+    tapte_dagsverk  numeric(17, 6) not null,
+    mulige_dagsverk numeric(17, 6) not null,
+    opprettet       timestamp default current_timestamp
 );
